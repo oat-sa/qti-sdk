2d50a4e6f78197458fda6e64b90b9ff5aeaae7a5
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-07-12 09:55:04 +0000

    appropriate decoration of XML Compact documents.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@15124 62dfeb86-a93f-4914-9d13-15c0a160985d

--- qtism/data/storage/xmlcompact/XmlCompactDocument.php
+++ qtism/data/storage/xmlcompact/XmlCompactDocument.php
@@ -4,6 +4,7 @@ namespace qtism\data\storage\xmlcompact;
 
 use qtism\data\storage\xml\XmlDocument;
 use qtism\data\storage\xmlcompact\marshalling\CompactMarshallerFactory;
+use \DOMElement;
 
 class XmlCompactDocument extends XmlDocument {
 	
@@ -15,5 +16,17 @@ class XmlCompactDocument extends XmlDocument {
 	 */
 	protected function createMarshallerFactory() {
 		return new CompactMarshallerFactory();
+	}
+	
+	/**
+	 * Override of XmlDocument.
+	 * 
+	 * Specifices the correct XSD schema locations and main namespace
+	 * for the root element of a Compact XML document.
+	 */
+	public function decorateRootElement(DOMElement $rootElement) {
+		$rootElement->setAttribute('xmlns', "http://www.imsglobal.org/xsd/imsqti_v2p1");
+		$rootElement->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+		$rootElement->setAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation', "http://www.taotesting.com/xsd/qticompact_v1p0.xsd");
 	}
 }
