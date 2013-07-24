93f23ba4ca7941054eddd538320a310c3bc74498
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-07-24 09:37:12 +0000

    GteProcessor implemented, tested, and approved.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@15193 62dfeb86-a93f-4914-9d13-15c0a160985d

--- qtism/runtime/expressions/processing/LteProcessor.php
+++ qtism/runtime/expressions/processing/LteProcessor.php
@@ -7,15 +7,15 @@ use qtism\data\expressions\Expression;
 use \InvalidArgumentException;
 
 /**
- * The LtProcessore class aims at processing Lte operators.
+ * The LteProcessor class aims at processing Lte operators.
  * 
  * From IMS QTI:
  * 
- * The lt operator takes two sub-expressions which must both have single cardinality 
+ * The lte operator takes two sub-expressions which must both have single cardinality 
  * and have a numerical base-type. The result is a single boolean with a value of 
- * true if the first expression is numerically less than the second and false if 
- * it is greater than or equal to the second. If either sub-expression is NULL 
- * then the operator results in NULL.
+ * true if the first expression is numerically less than or equal to the second and 
+ * false if it is greater than the second. If either sub-expression is NULL then the 
+ * operator results in NULL.
  * 
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  *
---
+++ test/qtism/runtime/expressions/processors/GteProcessorTest.php
@@ -0,0 +1,98 @@
+<?php
+require_once (dirname(__FILE__) . '/../../../../QtiSmTestCase.php');
+
+use qtism\runtime\common\RecordContainer;
+use qtism\common\datatypes\Point;
+use qtism\runtime\expressions\processing\GteProcessor;
+use qtism\runtime\expressions\processing\OperandsCollection;
+
+class GteProcessorTest extends QtiSmTestCase {
+	
+	public function testGte() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$operands[] = 1;
+		$operands[] = 0.5;
+		$processor = new GteProcessor($expression, $operands);
+		$result = $processor->process();
+		$this->assertInternalType('boolean', $result);
+		$this->assertTrue($result);
+		
+		$operands->reset();
+		$operands[] = 0.5;
+		$operands[] = 1;
+		$result = $processor->process();
+		$this->assertInternalType('boolean', $result);
+		$this->assertFalse($result);
+		
+		$operands->reset();
+		$operands[] = 1;
+		$operands[] = 1;
+		$result = $processor->process();
+		$this->assertInternalType('boolean', $result);
+		$this->assertTrue($result);
+	}
+	
+	public function testNull() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$operands[] = 1;
+		$operands[] = null;
+		$processor = new GteProcessor($expression, $operands);
+		$result = $processor->process();
+		$this->assertSame(null, $result);
+	}
+	
+	public function testWrongBaseTypeOne() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$operands[] = 1;
+		$operands[] = true;
+		$processor = new GteProcessor($expression, $operands);
+		$this->setExpectedException('qtism\\runtime\\expressions\\processing\\ExpressionProcessingException');
+		$result = $processor->process();
+	}
+	
+	public function testWrongBaseTypeTwo() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$operands[] = new Point(1, 2);
+		$operands[] = 2;
+		$processor = new GteProcessor($expression, $operands);
+		$this->setExpectedException('qtism\\runtime\\expressions\\processing\\ExpressionProcessingException');
+		$result = $processor->process();
+	}
+	
+	public function testWrongCardinality() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$operands[] = new RecordContainer(array('A' => 1));
+		$operands[] = 2;
+		$processor = new GteProcessor($expression, $operands);
+		$this->setExpectedException('qtism\\runtime\\expressions\\processing\\ExpressionProcessingException');
+		$result = $processor->process();
+	}
+	
+	public function testNotEnoughOperands() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection();
+		$this->setExpectedException('qtism\\runtime\\expressions\\processing\\ExpressionProcessingException');
+		$processor = new GteProcessor($expression, $operands);
+	}
+	
+	public function testTooMuchOperands() {
+		$expression = $this->createFakeExpression();
+		$operands = new OperandsCollection(array(1, 2, 3));
+		$this->setExpectedException('qtism\\runtime\\expressions\\processing\\ExpressionProcessingException');
+		$processor = new GteProcessor($expression, $operands);
+	}
+	
+	public function createFakeExpression() {
+		return $this->createComponentFromXml('
+			<gte>
+				<baseValue baseType="integer">10</baseValue>
+				<baseValue baseType="float">9.9</baseValue>
+			</gte>
+		');
+	}
+}
