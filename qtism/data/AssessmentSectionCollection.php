2bf1c468b1ce449e4b6629f1d91805ce0ff624d1
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-07-30 15:05:55 +0000

    AssessmentSectionCollection is now a QtiIdentifiableCollection.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@15243 62dfeb86-a93f-4914-9d13-15c0a160985d

--- qtism/data/AssessmentSectionCollection.php
+++ qtism/data/AssessmentSectionCollection.php
@@ -3,7 +3,6 @@
 namespace qtism\data;
 
 use InvalidArgumentException as InvalidArgumentException;
-use qtism\common\collections\AbstractCollection;
 
 /**
  * A collection that aims at storing AssessmentSection objects.
@@ -11,7 +10,7 @@ use qtism\common\collections\AbstractCollection;
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  *
  */
-class AssessmentSectionCollection extends AbstractCollection {
+class AssessmentSectionCollection extends QtiIdentifiableCollection {
 
 	/**
 	 * Check if $value is an AssessmentSection object.
@@ -19,6 +18,7 @@ class AssessmentSectionCollection extends AbstractCollection {
 	 * @throws InvalidArgumentException If $value is not a AssessmentSection object.
 	 */
 	protected function checkType($value) {
+		parent::checkType($value);
 		if (!$value instanceof AssessmentSection) {
 			$msg = "AssessmentSectionCollection class only accept AssessmentSection objects, '" . gettype($value) . "' given.";
 			throw new InvalidArgumentException($msg);
