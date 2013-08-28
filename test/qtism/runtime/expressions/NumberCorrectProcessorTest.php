063b2df889f0d20eb8a6bd428e45b01bf6ece58f
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-28 13:27:24 +0000

    NumberCorrect tested and approved.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@15612 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/qtism/runtime/expressions/NumberCorrectProcessorTest.php
+++ test/qtism/runtime/expressions/NumberCorrectProcessorTest.php
@@ -9,19 +9,19 @@ use qtism\common\enums\Cardinality;
 use qtism\runtime\common\ResponseVariable;
 use qtism\runtime\common\State;
 use qtism\common\collections\IdentifierCollection;
-use qtism\data\expressions\NumberResponded;
-use qtism\runtime\expressions\NumberRespondedProcessor;
+use qtism\data\expressions\NumberCorrect;
+use qtism\runtime\expressions\NumberCorrectProcessor;
 
 require_once (dirname(__FILE__) . '/../../../QtiSmItemSubsetTestCase.php');
 
-class NumberRespondedProcessorTest extends QtiSmItemSubsetTestCase {
+class NumberCorrectProcessorTest extends QtiSmItemSubsetTestCase {
 	
-	public function testNumberResponded() {
+	public function testNumberCorrect() {
 		$session = $this->getTestSession();
 		
-		$overallResponded = self::getNumberResponded();
-		$includeMathResponded = self::getNumberResponded('', new IdentifierCollection(array('mathematics')));
-		$processor = new NumberRespondedProcessor($overallResponded);
+		$overallCorrect = self::getNumberCorrect();
+		$includeMathResponded = self::getNumberCorrect('', new IdentifierCollection(array('mathematics')));
+		$processor = new NumberCorrectProcessor($overallCorrect);
 		$processor->setState($session);
 		
 		// Nothing responded yet.
@@ -35,7 +35,7 @@ class NumberRespondedProcessorTest extends QtiSmItemSubsetTestCase {
 		// Correct!
 		$responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::SINGLE, BaseType::IDENTIFIER, 'ChoiceA'));
 		$session->endAttempt($responses);
-		$processor->setExpression($overallResponded);
+		$processor->setExpression($overallCorrect);
 	    $this->assertEquals(1, $processor->process());
 	    $processor->setExpression($includeMathResponded);
 	    $this->assertEquals(1, $processor->process());
@@ -44,82 +44,88 @@ class NumberRespondedProcessorTest extends QtiSmItemSubsetTestCase {
 	    // Q02
 	    $responses->reset();
 	    $session->beginAttempt();
+	    // Incorrect!
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::MULTIPLE, BaseType::PAIR, new MultipleContainer(BaseType::PAIR, array(new Pair('A', 'P'), new Pair('D', 'L')))));
 	    $session->endAttempt($responses);
-	    $this->assertEquals(3, $session['Q02.SCORE']);  // just for fun...
-	    $processor->setExpression($overallResponded);
-	    $this->assertEquals(2, $processor->process());
+	    $processor->setExpression($overallCorrect);
+	    $this->assertEquals(1, $processor->process());
 	    $processor->setExpression($includeMathResponded);
 	    $this->assertEquals(1, $processor->process());
 	    $session->moveNext();
 	    
 	    // Q03
+	    // Incorrect!
 	    $session->beginAttempt();
 	    $session->skip();
-	    $processor->setExpression($overallResponded);
-	    $this->assertEquals(2, $processor->process());
+	    $processor->setExpression($overallCorrect);
+	    $this->assertEquals(1, $processor->process());
 	    
 	    // Q04
+	    // Correct!
 	    $responses->reset();
 	    $session->beginAttempt();
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::MULTIPLE, BaseType::DIRECTED_PAIR, new MultipleContainer(BaseType::DIRECTED_PAIR, array(new DirectedPair('W', 'G1'), new DirectedPair('Su', 'G2')))));
 	    $session->endAttempt($responses);
-	    $this->assertEquals(3, $session['Q04.SCORE']);
-	    $this->assertEquals(3, $processor->process());
+	    $this->assertEquals(2, $processor->process());
 	    $session->moveNext();
 	    
 	    // Q05
+	    // Incorrect!
 	    $session->beginAttempt();
 	    $session->skip();
-	    $this->assertEquals(3, $processor->process());
+	    $this->assertEquals(2, $processor->process());
 	    
 	    // Q06
+	    // Correct!
 	    $responses->reset();
 	    $session->beginAttempt();
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::SINGLE, BaseType::IDENTIFIER, 'A'));
 	    $session->endAttempt($responses);
-	    $this->assertEquals(4, $processor->process());
+	    $this->assertEquals(3, $processor->process());
 	    $session->moveNext();
 	    
 	    // Q07.1
+	    // Incorrect
 	    $responses->reset();
 	    $session->beginAttempt();
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::SINGLE, BaseType::POINT, new Point(100, 100)));
 	    $session->endAttempt($responses);
 	    $this->assertEquals(1, $session['Q07.1.SCORE']);
-	    $this->assertEquals(5, $processor->process());
+	    $this->assertEquals(3, $processor->process());
 	    $session->moveNext();
 	    
 	    // Q07.2
+	    // Incorrect!
 	    $responses->reset();
 	    $session->beginAttempt();
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::SINGLE, BaseType::POINT, new Point(10, 10)));
 	    $session->endAttempt($responses);
 	    $this->assertEquals(0, $session['Q07.2.SCORE']);
-	    $this->assertEquals(6, $processor->process());
+	    $this->assertEquals(3, $processor->process());
 	    $session->moveNext();
 	    
 	    // Q07.3
+	    // Correct!
 	    $responses->reset();
 	    $session->beginAttempt();
 	    $responses->setVariable(new ResponseVariable('RESPONSE', Cardinality::SINGLE, BaseType::POINT, new Point(102, 113)));
 	    $session->endAttempt($responses);
 	    $this->assertEquals(1, $session['Q07.3.SCORE']);
-	    $this->assertEquals(7, $processor->process());
+	    $this->assertEquals(4, $processor->process());
 	}
 	
-    protected static function getNumberResponded($sectionIdentifier = '', IdentifierCollection $includeCategories = null, IdentifierCollection $excludeCategories = null) {
-	    $numberResponded = new NumberResponded();
-	    $numberResponded->setSectionIdentifier($sectionIdentifier);
+    protected static function getNumberCorrect($sectionIdentifier = '', IdentifierCollection $includeCategories = null, IdentifierCollection $excludeCategories = null) {
+	    $numberCorrect = new NumberCorrect();
+	    $numberCorrect->setSectionIdentifier($sectionIdentifier);
 	    
 	    if (empty($includeCategories) === false) {
-	        $numberResponded->setIncludeCategories($includeCategories);
+	        $numberCorrect->setIncludeCategories($includeCategories);
 	    }
 	    
 	    if (empty($excludeCategories) === false) {
-	        $numberResponded->setExcludeCategories($excludeCategories);
+	        $numberCorrect->setExcludeCategories($excludeCategories);
 	    }
 
-	    return $numberResponded;
+	    return $numberCorrect;
 	}
 }
