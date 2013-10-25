e0950690b552affec6bf36476825260b7a41d9b9
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-10-25 10:54:16 +0000

    AssociableHotspot class (content model) and associates.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@16899 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ qtism/data/content/interactions/Hotspot.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ * @author Jérôme Bogaerts, <jerome@taotesting.com>
+ * @license GPLv2
+ * @package
+ */
+
+namespace qtism\data\content\interactions;
+
+use qtism\common\datatypes\Shape;
+use qtism\common\datatypes\Coords;
+use \InvalidArgumentException;
+
+/**
+ * From IMS QTI:
+ * 
+ * Some of the graphic interactions involve images with 
+ * specially defined areas or hotspots.
+ * 
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ *
+ */
+interface Hotspot {
+	
+	/**
+	 * Set the shape of the hotspot.
+	 * 
+	 * @param Shape $shape A Shape object.
+	 */
+	public function setShape(Shape $shape);
+	
+	/**
+	 * Get the shape of the hotspot.
+	 * 
+	 * @return Shape A Shape object.
+	 */
+	public function getShape();
+	
+	/**
+	 * Set the coords of the hotspot.
+	 * 
+	 * @param Coords $coords A Coords object.
+	 */
+	public function setCoords(Coords $coords);
+	
+	/**
+	 * Get the coords of the hotspot.
+	 * 
+	 * @return Coords A Coords object.
+	 */
+	public function getCoords();
+	
+	/**
+	 * Set the alternative text for this hotspot.
+	 * 
+	 * @param string $hotspotLabel A string with a maximum of 256 characters.
+	 * @throws InvalidArgumentException If $hotspotLabel is larger than 256 characters.
+	 */
+	public function setHotspotLabel($hotspotLabel);
+	
+	/**
+	 * Get the alternative text for this hotspot.
+	 * 
+	 * @return A string with a maximum of 256 characters.
+	 */
+	public function getHotspotLabel();
+}
