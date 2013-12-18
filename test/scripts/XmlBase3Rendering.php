0edbdec9bbaefb67d2be49321a5fafb4866222f4
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-12-18 14:11:10 +0000

    scripts added to check validity of xml:base mechanisms in rendering engine.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/QtiStateMachine/trunk@17648 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/scripts/XmlBase3Rendering.php
+++ test/scripts/XmlBase3Rendering.php
@@ -1,5 +1,6 @@
 <?php
 
+use qtism\runtime\rendering\AbstractRenderingEngine;
 use qtism\data\storage\xml\XmlDocument;
 use qtism\runtime\rendering\markup\xhtml\XhtmlRenderingEngine;
 
@@ -9,6 +10,22 @@ $doc = new XmlDocument();
 $doc->load('../samples/rendering/xmlbase_3.xml');
 
 $renderer = new XhtmlRenderingEngine();
+
+if (empty($argv[1]) === false) {
+    switch (strtolower($argv[1])) {
+        case 'ignore':
+            $renderer->setXmlBasePolicy(AbstractRenderingEngine::XMLBASE_IGNORE);
+            break;
+
+        case 'keep':
+            $renderer->setXmlBasePolicy(AbstractRenderingEngine::XMLBASE_KEEP);
+            break;
+
+        case 'process':
+            $renderer->setXmlBasePolicy(AbstractRenderingEngine::XMLBASE_PROCESS);
+            break;
+    }
+}
 
 $rendering = $renderer->render($doc->getDocumentComponent());
 $rendering->formatOutput = true;
