be4a00ce5b55d96964a3d3f8cea51480470fdd92
bugalood <jerome@taotesting.com>
2014-09-17 12:06:30 +0200

    InfoControl validation test.

--- test/qtismtest/data/storage/xml/XmlAssessmentItemDocumentTest.php
+++ test/qtismtest/data/storage/xml/XmlAssessmentItemDocumentTest.php
@@ -269,6 +269,7 @@ class XmlAssessmentItemDocumentTest extends QtiSmTestCase {
 		    
 		    // Other miscellaneous items...
 		    array(self::samplesDir() . 'custom/items/custom_operator_item.xml'),
+		    array(self::samplesDir() . 'custom/items/infocontrol.xml')
 		);
 	}
 	
