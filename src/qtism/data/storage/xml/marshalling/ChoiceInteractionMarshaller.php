1bb193804bf95e1b4f81b933127861d3bcf15505
bugalood <jerome@taotesting.com>
2015-01-12 15:59:16 +0100

    ChoiceInteractionMarshaller now correctly supports QTI 2.0.

--- src/qtism/data/storage/xml/marshalling/ChoiceInteractionMarshaller.php
+++ src/qtism/data/storage/xml/marshalling/ChoiceInteractionMarshaller.php
@@ -22,6 +22,7 @@
 
 namespace qtism\data\storage\xml\marshalling;
 
+use qtism\common\utils\Version;
 use qtism\data\content\interactions\OrderInteraction;
 use qtism\data\content\interactions\ChoiceInteraction;
 use qtism\data\content\interactions\Orientation;
@@ -43,68 +44,76 @@ class ChoiceInteractionMarshaller extends ContentMarshaller
      */
     protected function unmarshallChildrenKnown(DOMElement $element, QtiComponentCollection $children)
     {
-            if (($responseIdentifier = self::getDOMElementAttributeAs($element, 'responseIdentifier')) !== null) {
+        $version = $this->getVersion();
+        
+        if (($responseIdentifier = self::getDOMElementAttributeAs($element, 'responseIdentifier')) !== null) {
 
-                $fqClass = $this->lookupClass($element);
-                $component = new $fqClass($responseIdentifier, new SimpleChoiceCollection($children->getArrayCopy()));
+            $fqClass = $this->lookupClass($element);
+            $component = new $fqClass($responseIdentifier, new SimpleChoiceCollection($children->getArrayCopy()));
 
-                if (($shuffle = self::getDOMElementAttributeAs($element, 'shuffle', 'boolean')) !== null) {
-                    $component->setShuffle($shuffle);
-                }
-
-                if (($maxChoices = self::getDOMElementAttributeAs($element, 'maxChoices', 'integer')) !== null) {
-                    if ($element->localName === 'orderInteraction') {
-
-                        if ($maxChoices !== 0) {
-                            $component->setMaxChoices($maxChoices);
-                        }
-                    } else {
-                        $component->setMaxChoices($maxChoices);
-                    }
-                }
-
-                if (($minChoices = self::getDOMElementAttributeAs($element, 'minChoices', 'integer')) !== null) {
-                    if ($element->localName === 'orderInteraction') {
-                        /*
-                         * Lots of QTI implementations output minChoices = 0 while
-                         * dealing with orderInteraction unmarshalling. However, regarding
-                         * the IMS QTI Specification, it is invalid.
-                         *
-                         * "If specified, minChoices must be 1 or greater but must not exceed the
-                         * number of choices available."
-                         *
-                         * See http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10283
-                         */
-                        if ($minChoices !== 0) {
-                            $component->setMinChoices($minChoices);
-                        }
-                    } else {
-                        $component->setMinChoices($minChoices);
-                    }
-                }
-
-                if (($orientation = self::getDOMElementAttributeAs($element, 'orientation')) !== null) {
-                    $component->setOrientation(Orientation::getConstantByName($orientation));
-                }
-
-                if (($xmlBase = self::getXmlBase($element)) !== false) {
-                    $component->setXmlBase($xmlBase);
-                }
-
-                $promptElts = self::getChildElementsByTagName($element, 'prompt');
-                if (count($promptElts) > 0) {
-                    $promptElt = $promptElts[0];
-                    $prompt = $this->getMarshallerFactory()->createMarshaller($promptElt)->unmarshall($promptElt);
-                    $component->setPrompt($prompt);
-                }
-
-                self::fillBodyElement($component, $element);
-
-                return $component;
-            } else {
-                $msg = "The mandatory 'responseIdentifier' attribute is missing from the " . $element->localName . " element.";
+            if (($shuffle = self::getDOMElementAttributeAs($element, 'shuffle', 'boolean')) !== null) {
+                $component->setShuffle($shuffle);
+            } else if (Version::compare($version, '2.0.0', '==') === true && $element->localName === 'choiceInteraction') {
+                $msg = "The mandatory 'shuffle' attribute is missing from the " . $element->localName . " element.";
                 throw new UnmarshallingException($msg, $element);
             }
+
+            if (($maxChoices = self::getDOMElementAttributeAs($element, 'maxChoices', 'integer')) !== null) {
+                if ($element->localName === 'orderInteraction') {
+
+                    if ($maxChoices !== 0) {
+                        $component->setMaxChoices($maxChoices);
+                    }
+                } else {
+                    $component->setMaxChoices($maxChoices);
+                }
+            } else if (Version::compare($version, '2.0.0', '==') === true && $element->localName === 'choiceInteraction') {
+                $msg = "The mandatory 'maxChoices' attribute is missing from the " . $element->localName . " element.";
+                throw new UnmarshallingException($msg, $element);
+            }
+
+            if (($minChoices = self::getDOMElementAttributeAs($element, 'minChoices', 'integer')) !== null) {
+                if ($element->localName === 'orderInteraction') {
+                    /*
+                     * Lots of QTI implementations output minChoices = 0 while
+                     * dealing with orderInteraction unmarshalling. However, regarding
+                     * the IMS QTI Specification, it is invalid.
+                     *
+                     * "If specified, minChoices must be 1 or greater but must not exceed the
+                     * number of choices available."
+                     *
+                     * See http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10283
+                     */
+                    if ($minChoices !== 0) {
+                        $component->setMinChoices($minChoices);
+                    }
+                } else {
+                    $component->setMinChoices($minChoices);
+                }
+            }
+
+            if (($orientation = self::getDOMElementAttributeAs($element, 'orientation')) !== null) {
+                $component->setOrientation(Orientation::getConstantByName($orientation));
+            }
+
+            if (($xmlBase = self::getXmlBase($element)) !== false) {
+                $component->setXmlBase($xmlBase);
+            }
+
+            $promptElts = self::getChildElementsByTagName($element, 'prompt');
+            if (count($promptElts) > 0) {
+                $promptElt = $promptElts[0];
+                $prompt = $this->getMarshallerFactory()->createMarshaller($promptElt)->unmarshall($promptElt);
+                $component->setPrompt($prompt);
+            }
+
+            self::fillBodyElement($component, $element);
+
+            return $component;
+        } else {
+            $msg = "The mandatory 'responseIdentifier' attribute is missing from the " . $element->localName . " element.";
+            throw new UnmarshallingException($msg, $element);
+        }
     }
 
     /**
@@ -112,6 +121,8 @@ class ChoiceInteractionMarshaller extends ContentMarshaller
      */
     protected function marshallChildrenKnown(QtiComponent $component, array $elements)
     {
+        $version = $this->getVersion();
+        
         $element = self::getDOMCradle()->createElement($component->getQtiClassName());
         self::fillElement($element, $component);
         self::setDOMElementAttribute($element, 'responseIdentifier', $component->getResponseIdentifier());
@@ -120,11 +131,16 @@ class ChoiceInteractionMarshaller extends ContentMarshaller
             $element->appendChild($this->getMarshallerFactory()->createMarshaller($component->getPrompt())->marshall($component->getPrompt()));
         }
 
-        if ($component->mustShuffle() !== false) {
+        if ($component instanceof ChoiceInteraction && Version::compare($version, '2.0.0', '==') === true) {
+            self::setDOMElementAttribute($element, 'shuffle', $component->mustShuffle());
+        }
+        elseif ($component->mustShuffle() !== false) {
             self::setDOMElementAttribute($element, 'shuffle', true);
         }
 
-        if (($component instanceof ChoiceInteraction && $component->getMaxChoices() !== 1) || ($component instanceof OrderInteraction && $component->getMaxChoices() !== -1)) {
+        if ($component instanceof ChoiceInteraction && Version::compare($version, '2.0.0', '==') === true) {
+            self::setDOMElementAttribute($element, 'maxChoices', $component->getMaxChoices());
+        } elseif (($component instanceof ChoiceInteraction && $component->getMaxChoices() !== 1) || ($component instanceof OrderInteraction && $component->getMaxChoices() !== -1)) {
             self::setDOMElementAttribute($element, 'maxChoices', $component->getMaxChoices());
         }
 
