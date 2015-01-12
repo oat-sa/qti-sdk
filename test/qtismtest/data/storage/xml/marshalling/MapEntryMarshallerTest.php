a9466690bdb9c18ad5c6ecf3020153ca8116b14a
bugalood <jerome@taotesting.com>
2015-01-12 14:30:40 +0100

    MapEntryMarshaller now considers correctly version 2.0 of QTI.

--- test/qtismtest/data/storage/xml/marshalling/MapEntryMarshallerTest.php
+++ test/qtismtest/data/storage/xml/marshalling/MapEntryMarshallerTest.php
@@ -9,7 +9,7 @@ use \DOMDocument;
 
 class MapEntryMarshallerTest extends QtiSmTestCase {
 
-	public function testMarshall() {
+	public function testMarshall21() {
 
 		$component = new MapEntry(1337, 1.377, true);
 		
@@ -23,9 +23,9 @@ class MapEntryMarshallerTest extends QtiSmTestCase {
 		$this->assertEquals('true', $element->getAttribute('caseSensitive'));
 	}
 	
-	public function testUnmarshall() {
+	public function testUnmarshall21() {
 		$dom = new DOMDocument('1.0', 'UTF-8');
-		$dom->loadXML('<mapEntry xmlns="http://www.imsglobal.org/xsd/imsqti_v2p1" mapKey="1337" mappedValue="1.377" caseSensitive="true"/>');
+		$dom->loadXML('<mapEntry mapKey="1337" mappedValue="1.377" caseSensitive="true"/>');
 		$element = $dom->documentElement;
 		
 		$marshaller = $this->getMarshallerFactory('2.1.0')->createMarshaller($element, array(BaseType::INTEGER));
@@ -38,5 +38,42 @@ class MapEntryMarshallerTest extends QtiSmTestCase {
 		$this->assertEquals(1.377, $component->getMappedValue());
 		$this->assertInternalType('boolean', $component->isCaseSensitive());
 		$this->assertEquals(true, $component->isCaseSensitive());
+	}
+	
+	public function testMarshall20() {
+	    // No caseSensitive attribute in QTI 2.0. Check that no caseSensitive attribute goes out.
+	    $component = new MapEntry(1337, 1.377, true);
+		
+		$marshaller = $this->getMarshallerFactory('2.0.0')->createMarshaller($component, array(BaseType::INTEGER));
+		$element = $marshaller->marshall($component);
+		
+		$this->assertInstanceOf('\\DOMElement', $element);
+		$this->assertEquals('mapEntry', $element->nodeName);
+		$this->assertEquals('1337', $element->getAttribute('mapKey'));
+		$this->assertEquals('1.377', $element->getAttribute('mappedValue'));
+		
+		// Should return an empty string.
+		$this->assertSame('', $element->getAttribute('caseSensitive'));
+	}
+	
+	public function testUnmarshall20() {
+	    // Make sure the caseSensitive attribute is not taken into account.
+	    $dom = new DOMDocument('1.0', 'UTF-8');
+	    $dom->loadXML('<mapEntry mapKey="1337" mappedValue="1.377" caseSensitive="false"/>');
+	    $element = $dom->documentElement;
+	    
+	    $marshaller = $this->getMarshallerFactory('2.0.0')->createMarshaller($element, array(BaseType::INTEGER));
+	    $component = $marshaller->unmarshall($element);
+	    
+	    $this->assertInstanceOf('qtism\\data\\state\\MapEntry', $component);
+	    $this->assertInternalType('integer', $component->getMapKey());
+	    $this->assertEquals(1337, $component->getMapKey());
+	    $this->assertInternalType('float', $component->getMappedValue());
+	    $this->assertEquals(1.377, $component->getMappedValue());
+	    
+	    // Because default behaviour of the PHP model is true for caseSensitive,
+	    // make sure its not false.
+	    $this->assertInternalType('boolean', $component->isCaseSensitive());
+	    $this->assertEquals(true, $component->isCaseSensitive());
 	}
 }
