a5cef8ef3cbee7a474e3d71ec2feace5a906d2eb
bugalood <jerome@taotesting.com>
2015-03-19 11:46:15 +0100

    More unit testing for common\beans.

---
+++ test/qtismtest/common/beans/BeanPropertyTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace qtismtest\common\beans;
+
+use qtismtest\QtiSmTestCase;
+use qtism\common\beans\BeanException;
+use qtism\common\beans\BeanProperty;
+
+class BeanPropertyTest extends QtiSmTestCase {
+	
+    public function testNoProperty() {
+        $this->setExpectedException(
+            'qtism\\common\\beans\\BeanException',
+            "The class property with name 'prop' does not exist in class '\\stdClass'.",
+            BeanException::NO_PROPERTY
+        );
+        
+        $beanProperty = new BeanProperty('\\stdClass', 'prop');
+    }
+    
+    public function testPropertyNotAnnotated() {
+        $this->setExpectedException(
+            'qtism\\common\\beans\\BeanException',
+            "The property with name 'anotherUselessProperty' for class 'qtismtest\\common\\beans\\mocks\\SimpleBean' is not annotated.",
+            BeanException::NO_PROPERTY
+        );
+        
+        $beanProperty = new BeanProperty('qtismtest\\common\\beans\\mocks\\SimpleBean', 'anotherUselessProperty');
+    }
+}
