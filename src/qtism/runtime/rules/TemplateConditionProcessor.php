5c055592ee89c4be19f7e3a866dfe0a9387ac33f
bugalot <jerome@taotesting.com>
2015-04-03 17:35:37 +0200

    TemplateConditionProcessor.

---
+++ src/qtism/runtime/rules/TemplateConditionProcessor.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013-2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ * @license GPLv2
+ *
+ */
+
+namespace qtism\runtime\rules;
+
+/**
+ * From IMS QTI:
+ *
+ * If the expression given in the templateIf or templateElseIf evaluates to true then the sub-rules containe
+ * within it are followed and any following templateElseIf or templateElse parts are ignored for this template
+ * condition.
+ *
+ * If the expression given in the templateIf or templateElseIf does not evaluate to true then consideration
+ * passes to the next templateElseIf or, if there are no more templateElseIf parts then the sub-rules of
+ * the templateElse are followed (if specified).
+ *
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ *
+ */
+class TemplateConditionProcessor extends AbstractConditionProcessor
+{
+    /**
+     * @see \qtism\runtime\rules\AbstractConditionProcessor::getQtiNature()
+     */
+    public function getQtiNature()
+    {
+        return 'template';
+    }
+    
+    /**
+     * @see \qtism\runtime\rules\RuleProcessor::getRuleType()
+     */
+    protected function getRuleType()
+    {
+        return 'qtism\\data\\rules\\TemplateCondition';
+    }
+}
---
+++ test/qtismtest/runtime/rules/TemplateConditionProcessorTest.php
@@ -0,0 +1,125 @@
+<?php
+namespace qtismtest\runtime\rules;
+
+use qtismtest\QtiSmTestCase;
+use qtism\common\datatypes\Integer;
+use qtism\runtime\common\OutcomeVariable;
+use qtism\runtime\common\ResponseVariable;
+use qtism\runtime\rules\TemplateConditionProcessor;
+use qtism\common\enums\BaseType;
+use qtism\common\enums\Cardinality;
+use qtism\runtime\common\TemplateVariable;
+use qtism\runtime\common\State;
+
+class TemplateConditionProcessorTest extends QtiSmTestCase {
+	
+	public function testTemplateConditionSimpleIf1() {
+	    $rule = $this->createComponentFromXml('
+	        <templateCondition>
+    	        <templateIf>
+    	            <baseValue baseType="boolean">true</baseValue>
+    	            <setTemplateValue identifier="TPL1">
+    	                <baseValue baseType="integer">1</baseValue>
+    	            </setTemplateValue>
+    	        </templateIf>
+	        </templateCondition>
+	    ');
+	    
+	    $state = new State();
+	    $state->setVariable(new TemplateVariable('TPL1', Cardinality::SINGLE, BaseType::INTEGER));
+	    $processor = new TemplateConditionProcessor($rule);
+	    $processor->setState($state);
+	    $processor->process();
+	    
+	    $this->assertEquals(1, $state['TPL1']->getValue());
+	}
+	
+	/**
+	 * @depends testTemplateConditionSimpleIf1
+	 */
+	public function testTemplateConditionSimpleIf2() {
+	    $rule = $this->createComponentFromXml('
+	        <templateCondition>
+    	        <templateIf>
+    	            <baseValue baseType="boolean">true</baseValue>
+    	            <setTemplateValue identifier="TPL1">
+    	                <baseValue baseType="integer">1</baseValue>
+    	            </setTemplateValue>
+	                <setDefaultValue identifier="RSP1">
+    	                <baseValue baseType="identifier">Choice1</baseValue>
+    	            </setDefaultValue>
+	                <setCorrectResponse identifier="RSP1">
+	                    <baseValue baseType="identifier">Choice2</baseValue>
+	                </setCorrectResponse>
+    	        </templateIf>
+	        </templateCondition>
+	    ');
+	     
+	    $state = new State();
+	    
+	    $state->setVariable(new TemplateVariable('TPL1', Cardinality::SINGLE, BaseType::INTEGER));
+	    $state->setVariable(new ResponseVariable('RSP1', Cardinality::SINGLE, BaseType::IDENTIFIER));
+	    $processor = new TemplateConditionProcessor($rule);
+	    $processor->setState($state);
+	    $processor->process();
+	     
+	    $this->assertEquals(1, $state['TPL1']->getValue());
+	    $this->assertEquals('Choice1', $state->getVariable('RSP1')->getDefaultValue()->getValue());
+	    $this->assertEquals('Choice2', $state->getVariable('RSP1')->getCorrectResponse()->getValue());
+	}
+	
+	/**
+	 * @depends testTemplateConditionSimpleIf2
+	 * @dataProvider templateConditionSimpleIfElseIfElseProvider
+	 */
+	public function testTemplateConditionSimpleIfElseIfElse($controlValue, $expectedTpl1Value) {
+	    $rule = $this->createComponentFromXml('
+	        <templateCondition>
+    	        <templateIf>
+	                <match>
+    	                <variable identifier="CONTROL"/>
+	                    <baseValue baseType="integer">1</baseValue>
+	                </match>
+    	            <setTemplateValue identifier="TPL1">
+    	                <baseValue baseType="integer">1</baseValue>
+    	            </setTemplateValue>
+    	        </templateIf>
+	            <templateElseIf>
+	                <match>
+                        <variable identifier="CONTROL"/>
+	                    <baseValue baseType="integer">2</baseValue>
+	                </match>
+	                <setTemplateValue identifier="TPL1">
+	                    <baseValue baseType="integer">2</baseValue>
+	                </setTemplateValue>
+	            </templateElseIf>
+	            <templateElse>
+	                <setTemplateValue identifier="TPL1">
+	                    <baseValue baseType="integer">3</baseValue>
+	                </setTemplateValue>
+	            </templateElse>
+	        </templateCondition>
+	    ');
+	    
+	    $stateArray = array(
+	        new OutcomeVariable('CONTROL', Cardinality::SINGLE, BaseType::INTEGER, new Integer($controlValue)),
+	        new TemplateVariable('TPL1', Cardinality::SINGLE, BaseType::INTEGER)
+	    );
+	    $state = new State($stateArray);
+	    $processor = new TemplateConditionProcessor($rule);
+	    $processor->setState($state);
+	    $processor->process();
+	    
+	    $this->assertEquals($expectedTpl1Value, $state['TPL1']->getValue());
+	}
+	
+	public function templateConditionSimpleIfElseIfElseProvider() {
+	    return array(
+	        array(1, 1),
+	        array(2, 2),
+	        array(3, 3),
+	        array(4, 3),
+	        array(-1, 3)
+	    );
+	}
+}
