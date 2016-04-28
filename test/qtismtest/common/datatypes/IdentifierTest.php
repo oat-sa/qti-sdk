fc1cc18ef14094e91a05583658cb4adba56f0e61
= <jerome@taotesting.com>
2016-04-28 14:26:48 +0200

    No empty identifier allowed.

--- test/qtismtest/common/datatypes/IdentifierTest.php
+++ test/qtismtest/common/datatypes/IdentifierTest.php
@@ -7,7 +7,18 @@ use qtismtest\QtiSmTestCase;
 class IdentifierTest extends QtiSmTestCase {
     
     public function testWrongValue() {
-        $this->setExpectedException('\\InvalidArgumentException');
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            'The Identifier Datatype only accepts to store identifier values.'
+        );
         $float = new QtiIdentifier(1337);
+    }
+    
+    public function testEmptyIdentifier() {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            'The Identifier Datatype do not accept empty strings as valid identifiers.'
+        );
+        $float = new QtiIdentifier('');
     }
 }
