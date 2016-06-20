cceb0cde9ef6f78d11731ee6435f59bf5f66fa13
= <jerome@taotesting.com>
2016-06-20 17:13:43 +0200

    ResponseValidityConstraint in TextEntryInteraction.

--- src/qtism/data/content/interactions/TextEntryInteraction.php
+++ src/qtism/data/content/interactions/TextEntryInteraction.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -23,6 +23,7 @@
 namespace qtism\data\content\interactions;
 
 use qtism\data\QtiComponentCollection;
+use qtism\data\state\ResponseValidityConstraint;
 use qtism\common\utils\Format;
 use \InvalidArgumentException;
 
@@ -308,6 +309,14 @@ class TextEntryInteraction extends InlineInteraction implements StringInteractio
     public function hasPlaceholderText()
     {
         return $this->getPlaceholderText() !== '';
+    }
+
+    /**
+     * @see qtism\data\content\interactions\Interaction::getResponseValidityConstraint()
+     */
+    public function getResponseValidityConstraint()
+    {
+        return new ResponseValidityConstraint(0, 1, $this->getPatternMask());
     }
 
     /**
