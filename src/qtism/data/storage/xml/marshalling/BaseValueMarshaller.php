34deee8b057fe237419eb5500ff5ab21ed0eb092
= <jerome@taotesting.com>
2016-06-21 14:55:41 +0200

    Trim values in BaseValueMarshaller if not a string.

--- src/qtism/data/storage/xml/marshalling/BaseValueMarshaller.php
+++ src/qtism/data/storage/xml/marshalling/BaseValueMarshaller.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -65,6 +65,12 @@ class BaseValueMarshaller extends Marshaller
 
             $value = $element->nodeValue;
             $baseTypeCst = BaseType::getConstantByName($baseType);
+            
+            // A little bit of cleaning...
+            if ($baseTypeCst !== BaseType::STRING) {
+                $value = trim($value);
+            }
+            
             $object = new BaseValue($baseTypeCst, Utils::stringToDatatype($value, $baseTypeCst));
 
             return $object;
