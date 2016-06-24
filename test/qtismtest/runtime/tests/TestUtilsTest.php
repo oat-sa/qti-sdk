fce0d051d83d3b8cf850aa228655f1007710be53
= <jerome@taotesting.com>
2016-06-24 17:54:15 +0200

    More isResponseValid testing...

--- test/qtismtest/runtime/tests/TestUtilsTest.php
+++ test/qtismtest/runtime/tests/TestUtilsTest.php
@@ -2,7 +2,12 @@
 namespace qtismtest\runtime\tests;
 
 use qtismtest\QtiSmTestCase;
+use qtism\common\enums\BaseType;
 use qtism\common\datatypes\QtiDatatype;
+use qtism\common\datatypes\QtiString;
+use qtism\common\datatypes\QtiInteger;
+use qtism\runtime\common\MultipleContainer;
+use qtism\runtime\common\OrderedContainer;
 use qtism\data\state\ResponseValidityConstraint;
 use qtism\runtime\tests\Utils as TestUtils;
 
@@ -22,6 +27,18 @@ class TestUtilsTest extends QtiSmTestCase {
             array(true, null, new ResponseValidityConstraint('RESPONSE', 0, 3)),
             array(false, null, new ResponseValidityConstraint('RESPONSE', 1, 3)),
             array(false, null, new ResponseValidityConstraint('RESPONSE', 2, 3)),
+            array(false, null, new ResponseValidityConstraint('RESPONSE', 1, 1)),
+            array(false, new QtiString(''), new ResponseValidityConstraint('RESPONSE', 1, 1)),
+            
+            array(true, new QtiString('string!'), new ResponseValidityConstraint('RESPONSE', 1, 1)),
+            array(true, new QtiInteger(1337), new ResponseValidityConstraint('RESPONSE', 1, 0)),
+            
+            array(true, new MultipleContainer(BaseType::INTEGER, array(new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 1, 1)),
+            array(true, new MultipleContainer(BaseType::INTEGER, array(new QtiInteger(1337), new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 1, 2)),
+            array(true, new OrderedContainer(BaseType::INTEGER, array(new QtiInteger(1337), new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 1, 2)),
+            array(false, new OrderedContainer(BaseType::INTEGER, array(new QtiInteger(1337), new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 1, 1)),
+            array(false, new OrderedContainer(BaseType::INTEGER, array(new QtiInteger(1337), new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 0, 1)),
+            array(true, new OrderedContainer(BaseType::INTEGER, array(new QtiInteger(1337), new QtiInteger(1337))), new ResponseValidityConstraint('RESPONSE', 0, 0)),
         );
     }
 }
