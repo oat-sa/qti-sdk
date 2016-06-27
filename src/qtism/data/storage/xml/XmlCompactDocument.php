da5a7e53b9ba53b064904a692681c84395f5ae54
= <jerome@taotesting.com>
2016-06-27 11:30:13 +0200

    Retrieve ResponseValidityConstraints when Compacting tests.

--- src/qtism/data/storage/xml/XmlCompactDocument.php
+++ src/qtism/data/storage/xml/XmlCompactDocument.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2013-2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -309,6 +309,7 @@ class XmlCompactDocument extends XmlDocument
             $compactAssessmentItemRef->setAdaptive($item->isAdaptive());
             $compactAssessmentItemRef->setTimeDependent($item->isTimeDependent());
             $compactAssessmentItemRef->setEndAttemptIdentifiers($item->getEndAttemptIdentifiers());
+            $compactAssessmentItemRef->setResponseValidityConstraints($item->getResponseValidityConstraints());
         } catch (Exception $e) {
             $msg = "An error occured while unreferencing item reference with identifier '" . $compactAssessmentItemRef->getIdentifier() . "'.";
             throw new XmlStorageException($msg, XmlStorageException::RESOLUTION, $e);
