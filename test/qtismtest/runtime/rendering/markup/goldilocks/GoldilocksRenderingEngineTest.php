fd631e7bd11714f37e24bc9f9884ce19b2658e4b
= <jerome@taotesting.com>
2016-07-25 11:48:35 +0200

    More unit tests on GoldilocksRenderingEngine.

--- test/qtismtest/runtime/rendering/markup/goldilocks/GoldilocksRenderingEngineTest.php
+++ test/qtismtest/runtime/rendering/markup/goldilocks/GoldilocksRenderingEngineTest.php
@@ -53,13 +53,63 @@ class GoldilocksRenderingEngineTest extends QtiSmTestCase {
             ),
             // choiceInteraction-2
             array(
+                self::samplesDir() . 'ims/items/2_2/choice_fixed.xml',
+                self::samplesDir() . 'rendering/goldilocks/rendered/choiceInteraction-2.html',
+                GoldilocksRenderingEngine::CONTEXT_STATIC,
+                GoldilocksRenderingEngine::XMLBASE_IGNORE,
+                GoldilocksRenderingEngine::STYLESHEET_INLINE,
+                GoldilocksRenderingEngine::CSSCLASS_CONCRETE
+            ),
+            // choiceInteraction-3
+            array(
+                self::samplesDir() . 'ims/items/2_2/choice_multiple_rtl.xml',
+                self::samplesDir() . 'rendering/goldilocks/rendered/choiceInteraction-3.html',
+                GoldilocksRenderingEngine::CONTEXT_STATIC,
+                GoldilocksRenderingEngine::XMLBASE_IGNORE,
+                GoldilocksRenderingEngine::STYLESHEET_INLINE,
+                GoldilocksRenderingEngine::CSSCLASS_CONCRETE
+            ),
+            // associateInteraction-0
+            array(
                 self::samplesDir() . 'ims/items/2_2/associate.xml',
                 self::samplesDir() . 'rendering/goldilocks/rendered/associateInteraction-0.html',
                 GoldilocksRenderingEngine::CONTEXT_STATIC,
                 GoldilocksRenderingEngine::XMLBASE_IGNORE,
                 GoldilocksRenderingEngine::STYLESHEET_INLINE,
                 GoldilocksRenderingEngine::CSSCLASS_CONCRETE
-            )
+            ),
+            // extendedTextInteraction-0
+            array(
+                self::samplesDir() . 'ims/items/2_2/extended_text.xml',
+                self::samplesDir() . 'rendering/goldilocks/rendered/extendedTextInteraction-0.html',
+                GoldilocksRenderingEngine::CONTEXT_STATIC,
+                GoldilocksRenderingEngine::XMLBASE_IGNORE,
+                GoldilocksRenderingEngine::STYLESHEET_INLINE,
+                GoldilocksRenderingEngine::CSSCLASS_CONCRETE
+            ),
         );
     }
+    
+    /*public function testGenerate() {
+        $renderingMode = GoldilocksRenderingEngine::CONTEXT_STATIC;
+        $xmlBasePolicy = GoldilocksRenderingEngine::XMLBASE_IGNORE;
+        $stylesheetPolicy = GoldilocksRenderingEngine::STYLESHEET_INLINE;
+        $cssClassPolicy = GoldilocksRenderingEngine::CSSCLASS_CONCRETE;
+        
+        $engine = new GoldilocksRenderingEngine();
+        $engine->setChoiceShowHidePolicy($renderingMode);
+        $engine->setFeedbackShowHidePolicy($renderingMode);
+        $engine->setViewPolicy($renderingMode);
+        $engine->setPrintedVariablePolicy($renderingMode);
+        $engine->setXmlBasePolicy($xmlBasePolicy);
+        $engine->setStylesheetPolicy($stylesheetPolicy);
+        $engine->setCssClassPolicy($cssClassPolicy);
+        
+        $doc = new XmlDocument();
+        $doc->load(self::samplesDir() . 'ims/items/2_2/extended_text_rubric.xml');
+        
+        $rendered = $engine->render($doc->getDocumentComponent());
+        $strRendered = $rendered->saveXML($rendered->documentElement);
+        file_put_contents(self::samplesDir() . 'rendering/goldilocks/rendered/extendedText-1.html', $strRendered . "\n");
+    }*/
 }
