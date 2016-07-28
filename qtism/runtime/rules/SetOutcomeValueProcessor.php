d8495ac9f798bf7713bb76a242c1faf490cacdff
= <jerome@taotesting.com>
2016-07-28 15:09:57 +0200

    Stronger type juggling in setOutcomeValue rule processor.

--- qtism/runtime/rules/SetOutcomeValueProcessor.php
+++ qtism/runtime/rules/SetOutcomeValueProcessor.php
@@ -116,6 +116,11 @@ class SetOutcomeValueProcessor extends RuleProcessor {
 		    if ($val !== null && $var->getCardinality() === Cardinality::SINGLE) {
 		        $baseType = $var->getBaseType();
 		        
+                // If we are trying to put a container in a scalar, let's make some changes...
+                if (($val->getCardinality() === Cardinality::MULTIPLE || $val->getCardinality() === Cardinality::ORDERED) && count($val) > 0) {
+                    $val = $val[0];
+                }
+                
 		        if ($baseType === BaseType::INTEGER && $val instanceof QtiFloat) {
 		            $val = new QtiInteger(intval($val->getValue()));
 		        }
