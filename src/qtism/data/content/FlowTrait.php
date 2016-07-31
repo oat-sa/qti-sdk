ee4518b275749626025110fba06e9bcc601f2fa9
= <jerome@taotesting.com>
2016-07-31 17:45:52 +0200

    FlowTrait creation.

---
+++ src/qtism/data/content/FlowTrait.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Copyright (c) 2013-2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ * @license GPLv2
+ */
+
+namespace qtism\data\content;
+
+use \InvalidArgumentException;
+
+/**
+ * The Flow trait.
+ *
+ * @authorJérôme Bogaerts <jerome@taotesting.com>
+ * @see \qtism\data\content\Flow
+ */
+trait FlowTrait
+{
+    private $xmlBase = '';
+    
+    /**
+     * setXmlBase method implementation.
+     * 
+     * @see \qtism\data\content\Flow::setXmlBase()
+     */
+    public function setXmlBase($base = '')
+    {
+        if (is_string($xmlBase) && (empty($xmlBase) || Format::isUri($xmlBase))) {
+            $this->xmlBase = $xmlBase;
+        } else {
+            $msg = "The 'xmlBase' argument must be an empty string or a valid URI, '" . $xmlBase . "' given";
+            throw new InvalidArgumentException($msg);
+        }
+    }
+
+    /**
+     * getXmlBase method implementation.
+     * 
+     * @see \qtism\data\content\Flow::getXmlBase()
+     */
+    public function getXmlBase()
+    {
+        return $this->xmlBase;
+    }
+
+    /**
+     * hasXmlBase method implementation.
+     * 
+     * @see \qtism\data\content\Flow::hasXmlBase()
+     */
+    public function hasXmlBase()
+    {
+        return $this->getXmlBase() !== '';
+    }
+}
