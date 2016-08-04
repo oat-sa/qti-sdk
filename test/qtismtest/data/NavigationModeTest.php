5ebb9575e7b7bead48b87803c3545b0091f635d3
= <jerome@taotesting.com>
2016-08-04 16:25:57 +0200

    New enumerations tests.

---
+++ test/qtismtest/data/NavigationModeTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace qtismtest\data;
+
+use qtismtest\QtiSmEnumTestCase;
+use qtism\data\NavigationMode;
+
+class NavigationModeTest extends QtiSmEnumTestCase
+{
+    protected function getEnumerationFqcn()
+    {
+        return NavigationMode::class;
+    }
+    
+    protected function getNames()
+    {
+        return array(
+            'linear',
+            'nonlinear'
+        );
+    }
+    
+    protected function getKeys()
+    {
+        return array(
+            'LINEAR',
+            'NONLINEAR'
+        );
+    }
+    
+    protected function getConstants()
+    {
+        return array(
+            NavigationMode::LINEAR,
+            NavigationMode::NONLINEAR
+        );
+    }
+}
