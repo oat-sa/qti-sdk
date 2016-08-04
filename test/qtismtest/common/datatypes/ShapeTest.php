94c6e961b1998cb6804f8f6036e92f6f961baf93
= <jerome@taotesting.com>
2016-08-04 14:29:10 +0200

    Tests for QtiShape and BaseType enums.

---
+++ test/qtismtest/common/datatypes/ShapeTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace qtismtest\common\datatypes;
+
+use qtismtest\QtiSmEnumTestCase;
+use qtism\common\datatypes\QtiShape;
+
+class ShapeTest extends QtiSmEnumTestCase
+{
+    protected function getEnumerationFqcn()
+    {
+        return QtiShape::class;
+    }
+    
+    protected function getNames()
+    {
+        return array(
+            'default',
+            'rect',
+            'circle',
+            'poly',
+            'ellipse'
+        );
+    }
+    
+    protected function getKeys()
+    {
+        return array(
+            'DEF',
+            'RECT',
+            'CIRCLE',
+            'POLY',
+            'ELLIPSE'
+        );
+    }
+    
+    protected function getConstants()
+    {
+        return array(
+            QtiShape::DEF,
+            QtiShape::RECT,
+            QtiShape::CIRCLE,
+            QtiShape::POLY,
+            QtiShape::ELLIPSE
+        );
+    }
+}
