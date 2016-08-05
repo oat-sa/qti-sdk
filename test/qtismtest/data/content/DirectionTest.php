62df152eb85f3b204fb8045e9f48f34f2072fba3
= <jerome@taotesting.com>
2016-08-05 14:47:56 +0200

    Direction enumeration test.

---
+++ test/qtismtest/data/content/DirectionTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace qtismtest\data\content;
+
+use qtismtest\QtiSmEnumTestCase;
+use qtism\data\content\Direction;
+
+class DirectionTest extends QtiSmEnumTestCase
+{
+    protected function getEnumerationFqcn()
+    {
+        return Direction::class;
+    }
+    
+    protected function getNames()
+    {
+        return array(
+            'auto',
+            'ltr',
+            'rtl'
+        );
+    }
+    
+    protected function getKeys()
+    {
+        return array(
+            'AUTO',
+            'LTR',
+            'RTL'
+        );
+    }
+    
+    protected function getConstants()
+    {
+        return array(
+            Direction::AUTO,
+            Direction::LTR,
+            Direction::RTL,
+        );
+    }
+}
