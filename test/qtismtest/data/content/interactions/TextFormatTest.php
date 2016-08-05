e92f4703badaceb71e8d6f0b8fd624859539d969
= <jerome@taotesting.com>
2016-08-05 14:44:22 +0200

    TextFormat enumeration test.

---
+++ test/qtismtest/data/content/interactions/TextFormatTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace qtismtest\data\content\interactions;
+
+use qtismtest\QtiSmEnumTestCase;
+use qtism\data\content\interactions\TextFormat;
+
+class TextFormatTest extends QtiSmEnumTestCase
+{
+    protected function getEnumerationFqcn()
+    {
+        return TextFormat::class;
+    }
+    
+    protected function getNames()
+    {
+        return array(
+            'plain',
+            'preFormatted',
+            'xhtml'
+        );
+    }
+    
+    protected function getKeys()
+    {
+        return array(
+            'PLAIN',
+            'PRE_FORMATTED',
+            'XHTML'
+        );
+    }
+    
+    protected function getConstants()
+    {
+        return array(
+            TextFormat::PLAIN,
+            TextFormat::PRE_FORMATTED,
+            TextFormat::XHTML
+        );
+    }
+}
