e05c38d322900f37ac356f5bcb87f7eb7efe7e03
= <jerome@taotesting.com>
2016-08-13 16:48:55 +0200

    Context Aware rendering test for PrintedVariable.

--- test/qtismtest/runtime/rendering/markup/xhtml/PrintedVariableRendererTest.php
+++ test/qtismtest/runtime/rendering/markup/xhtml/PrintedVariableRendererTest.php
@@ -2,13 +2,18 @@
 namespace qtismtest\runtime\rendering\markup\xhtml;
 
 use qtismtest\QtiSmTestCase;
+use qtism\common\enums\Cardinality;
+use qtism\common\enums\BaseType;
+use qtism\common\datatypes\QtiFloat;
 use qtism\data\content\PrintedVariable;
+use qtism\runtime\common\State;
+use qtism\runtime\common\OutcomeVariable;
 use qtism\runtime\rendering\markup\xhtml\PrintedVariableRenderer;
 use qtism\runtime\rendering\markup\xhtml\XhtmlRenderingEngine;
 
 class PrintedVariableRendererTest extends QtiSmTestCase 
 {
-    public function testRenderNoChildren() {
+    public function testRenderContextStatic() {
         $ctx = new XhtmlRenderingEngine();
         $pv = new PrintedVariable('OUTCOME1', 'my-id');
         $pv->setFormat('%d');
@@ -34,5 +39,33 @@ class PrintedVariableRendererTest extends QtiSmTestCase
         $this->assertEquals(',', $element->getAttribute('data-delimiter'));
         $this->assertEquals('field', $element->getAttribute('data-field'));
         $this->assertEquals('=', $element->getAttribute('data-mapping-indicator'));
+    }
+    
+    public function testRenderContextAware() {
+        $ctx = new XhtmlRenderingEngine();
+        $ctx->setPrintedVariablePolicy(XhtmlRenderingEngine::CONTEXT_AWARE);
+        
+        $var = new OutcomeVariable('OUTCOME1', Cardinality::SINGLE, BaseType::FLOAT);
+        $var->setValue(new QtiFloat(1.337));
+        $state = new State(array($var));
+        $ctx->setState($state);
+        
+        $pv = new PrintedVariable('OUTCOME1', 'my-id');
+        $pv->setFormat('%d');
+        
+        $renderer = new PrintedVariableRenderer();
+        $renderer->setRenderingEngine($ctx);
+        
+        $element = $renderer->render($pv)->firstChild;
+        
+        $this->assertEquals('span', $element->nodeName);
+        $this->assertEquals('my-id', $element->getAttribute('id'));
+        $this->assertEquals('OUTCOME1', $element->getAttribute('data-identifier'));
+        $this->assertEquals('%d', $element->getAttribute('data-format'));
+        $this->assertEquals('false', $element->getAttribute('data-power-form'));
+        $this->assertEquals('10', $element->getAttribute('data-base'));
+        $this->assertEquals(';', $element->getAttribute('data-delimiter'));
+        $this->assertEquals('=', $element->getAttribute('data-mapping-indicator'));
+        $this->assertEquals('qti-printedVariable', $element->getAttribute('class'));
     }
 }
