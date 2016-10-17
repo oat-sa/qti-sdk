bd030f04627110f2fd80be4d399a7653d9125548
bugalot <jerome@taotesting.com>
2016-10-17 16:53:43 +0200

    Additional unit tests for LookupOutcomeValue.

--- test/qtismtest/runtime/rules/LookupOutcomeValueProcessorTest.php
+++ test/qtismtest/runtime/rules/LookupOutcomeValueProcessorTest.php
@@ -4,14 +4,13 @@ namespace qtismtest\runtime\rules;
 use qtismtest\QtiSmTestCase;
 use qtism\common\datatypes\QtiPair;
 use qtism\runtime\common\State;
-use qtism\common\enums\BaseType;
-use qtism\common\enums\Cardinality;
 use qtism\runtime\common\OutcomeVariable;
 use qtism\runtime\rules\LookupOutcomeValueProcessor;
 
-class LookupOutcomeValueProcessorTest extends QtiSmTestCase {
-	
-	public function testLookupOutcomeValueSimpleMatchTable() {
+class LookupOutcomeValueProcessorTest extends QtiSmTestCase
+{
+	public function testLookupOutcomeValueSimpleMatchTable()
+    {
 		$rule = $this->createComponentFromXml('
 			<lookupOutcomeValue identifier="outcome1">
 				<baseValue baseType="integer">2</baseValue>
@@ -47,7 +46,8 @@ class LookupOutcomeValueProcessorTest extends QtiSmTestCase {
 		$this->assertTrue($state['outcome1']->equals(new QtiPair('Y', 'Z')));
 	}
 	
-	public function testLookupOutcomeValueSimpleInterpolationTable() {
+	public function testLookupOutcomeValueSimpleInterpolationTable()
+    {
 		$rule = $this->createComponentFromXml('
 			<lookupOutcomeValue identifier="outcome1">
 				<baseValue baseType="float">2.0</baseValue>
@@ -90,4 +90,111 @@ class LookupOutcomeValueProcessorTest extends QtiSmTestCase {
 		$this->assertInstanceOf('qtism\\common\\datatypes\\QtiString', $state['outcome1']);
 		$this->assertEquals("What's going on?", $state['outcome1']->getValue());
 	}
+    
+    public function testLookupOutcomeValueNoVariable()
+    {
+        $rule = $this->createComponentFromXml('
+			<lookupOutcomeValue identifier="outcome1">
+				<baseValue baseType="integer">2</baseValue>
+			</lookupOutcomeValue>
+		');
+        
+        $processor = new LookupOutcomeValueProcessor($rule);
+        $state = new State();
+        $processor->setState($state);
+        
+        $this->setExpectedException(
+            'qtism\\runtime\\rules\\RuleProcessingException',
+            "The variable to set 'outcome1' does not exist in the current state."
+        );
+        
+        $processor->process();
+    }
+    
+    public function testLookupOutcomeValueNoLookupTable()
+    {
+        $rule = $this->createComponentFromXml('
+			<lookupOutcomeValue identifier="outcome1">
+				<baseValue baseType="float">2.0</baseValue>
+			</lookupOutcomeValue>
+		');
+        
+        $declaration = $this->createComponentFromXml('
+			<outcomeDeclaration identifier="outcome1" cardinality="single" baseType="string"/>
+		');
+        
+        $outcome = OutcomeVariable::createFromDataModel($declaration);
+        $state = new State(array($outcome));
+        $processor = new LookupOutcomeValueProcessor($rule);
+        $processor->setState($state);
+        
+        $this->setExpectedException(
+            'qtism\\runtime\\rules\\RuleProcessingException',
+            "No lookupTable in declaration of variable 'outcome1'."
+        );
+        
+        $processor->process();
+    }
+    
+    public function testLookupOutcomeWrongLookupValueTypeForInterpolationTable()
+    {
+        $rule = $this->createComponentFromXml('
+			<lookupOutcomeValue identifier="outcome1">
+				<baseValue baseType="boolean">true</baseValue>
+			</lookupOutcomeValue>
+		');
+        
+        $declaration = $this->createComponentFromXml('
+			<outcomeDeclaration identifier="outcome1" cardinality="single" baseType="string">
+			    <interpolationTable defaultValue="What\'s going on?">
+					<interpolationTableEntry sourceValue="1.0" includeBoundary="true" targetValue="Come get some!"/>
+					<interpolationTableEntry sourceValue="2.0" includeBoundary="false" targetValue="Piece of cake!"/>
+					<interpolationTableEntry sourceValue="3.0" includeBoundary="true" targetValue="Awesome!"/>
+				</interpolationTable>
+			</outcomeDeclaration>
+		');
+        
+        $outcome = OutcomeVariable::createFromDataModel($declaration);
+        $state = new State(array($outcome));
+        $processor = new LookupOutcomeValueProcessor($rule);
+        $processor->setState($state);
+        
+        $this->setExpectedException(
+            'qtism\\runtime\\rules\\RuleProcessingException',
+            "The value of variable 'outcome1' must be integer, float or duration when used with an interpolationTable"
+        );
+        
+        $processor->process();
+    }
+    
+    public function testLookupOutcomeWrongLookupValueTypeForMatchTable()
+    {
+        $rule = $this->createComponentFromXml('
+			<lookupOutcomeValue identifier="outcome1">
+				<baseValue baseType="boolean">true</baseValue>
+			</lookupOutcomeValue>
+		');
+    
+        $declaration = $this->createComponentFromXml('
+			<outcomeDeclaration identifier="outcome1" cardinality="single" baseType="string">
+			    <matchTable defaultValue="What\'s going on?">
+					<matchTableEntry sourceValue="1" targetValue="Come get some!"/>
+					<matchTableEntry sourceValue="2" targetValue="Piece of cake!"/>
+					<matchTableEntry sourceValue="3" targetValue="Awesome!"/>
+				</matchTable>
+			</outcomeDeclaration>
+		');
+    
+        $outcome = OutcomeVariable::createFromDataModel($declaration);
+        $state = new State(array($outcome));
+        $processor = new LookupOutcomeValueProcessor($rule);
+        $processor->setState($state);
+    
+        $this->setExpectedException(
+            'qtism\\runtime\\rules\\RuleProcessingException',
+            "The value of the variable 'outcome1' must be integer when used with a matchTable."
+        );
+    
+        $processor->process();
+    }
 }
