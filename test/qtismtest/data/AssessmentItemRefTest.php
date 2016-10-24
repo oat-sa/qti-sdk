5dcff7b2e9aee64d95d87d7b19e12a392d0e8458
= <jerome@taotesting.com>
2016-10-24 13:46:45 +0200

    Additional unit tests for AssessmentItemRef.

--- test/qtismtest/data/AssessmentItemRefTest.php
+++ test/qtismtest/data/AssessmentItemRefTest.php
@@ -3,6 +3,14 @@ namespace qtismtest\data;
 
 use qtismtest\QtiSmTestCase;
 use qtism\data\AssessmentItemRef;
+use qtism\data\ItemSessionControl;
+use qtism\data\TimeLimits;
+use qtism\data\rules\BranchRule;
+use qtism\data\rules\BranchRuleCollection;
+use qtism\data\rules\PreCondition;
+use qtism\data\rules\PreConditionCollection;
+use qtism\data\expressions\BaseValue;
+use qtism\common\enums\BaseType;
 
 class AssessmentItemRefTest extends QtiSmTestCase
 {
@@ -38,5 +46,31 @@ class AssessmentItemRefTest extends QtiSmTestCase
         );
         
         $assessmentItemRef->setFixed('test');
+    }
+    
+    public function testClone()
+    {
+        $assessmentItemRef = new AssessmentItemRef('Q01', 'Q01.xml');
+        $itemSessionControl = new ItemSessionControl();
+        $assessmentItemRef->setItemSessionControl($itemSessionControl);
+        
+        $timeLimits = new TimeLimits();
+        $assessmentItemRef->setTimeLimits($timeLimits);
+        
+        $branchRule = new BranchRule(new BaseValue(BaseType::BOOLEAN, true), 'Q01');
+        $preCondition = new PreCondition(new BaseValue(BaseType::BOOLEAN, true));
+        
+        $branchRules = new BranchRuleCollection([$branchRule]);
+        $preConditions = new PreconditionCollection([$preCondition]);
+        $assessmentItemRef->setBranchRules($branchRules);
+        $assessmentItemRef->setPreConditions($preConditions);
+        
+        $cloneAssessmentItemRef = clone $assessmentItemRef;
+        
+        $this->assertNotSame($assessmentItemRef, $cloneAssessmentItemRef);
+        $this->assertNotSame($itemSessionControl, $cloneAssessmentItemRef->getItemSessionControl());
+        $this->assertNotSame($timeLimits, $cloneAssessmentItemRef->getTimeLimits());
+        $this->assertNotSame($preConditions, $cloneAssessmentItemRef->getPreConditions());
+        $this->assertNotSame($branchRules, $cloneAssessmentItemRef->getBranchRules());
     }
 }
