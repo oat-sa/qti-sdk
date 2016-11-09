3c4ab7848c6eb4a8c722efbb7686387f0b1cf7cd
= <jerome@taotesting.com>
2016-11-09 05:19:02 +0100

    Additional unit tests for EndAttemptInteractionMarshaller.

--- src/qtism/data/content/interactions/EndAttemptInteraction.php
+++ src/qtism/data/content/interactions/EndAttemptInteraction.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -86,7 +86,7 @@ class EndAttemptInteraction extends InlineInteraction
      */
     public function setTitle($title)
     {
-        if (is_string($title) === true && empty($title) === false) {
+        if (is_string($title) === true) {
             $this->title = $title;
         } else {
             $msg = "The 'title' argument must be a string, '" . gettype($title) . "' given.";
--- test/qtismtest/data/storage/xml/marshalling/EndAttemptInteractionMarshallerTest.php
+++ test/qtismtest/data/storage/xml/marshalling/EndAttemptInteractionMarshallerTest.php
@@ -5,20 +5,23 @@ use qtismtest\QtiSmTestCase;
 use qtism\data\content\interactions\EndAttemptInteraction;
 use \DOMDocument;
 
-class EndAttemptInteractionMarshallerTest extends QtiSmTestCase {
-
-	public function testMarshall() {
+class EndAttemptInteractionMarshallerTest extends QtiSmTestCase
+{
+	public function testMarshall()
+    {
 	    $endAttemptInteraction = new EndAttemptInteraction('BOOL_RESP', 'End the attempt now!', 'my-end', 'ending');
+        $endAttemptInteraction->setXmlBase('/home/jerome');
         $element = $this->getMarshallerFactory('2.1.0')->createMarshaller($endAttemptInteraction)->marshall($endAttemptInteraction);
         
         $dom = new DOMDocument('1.0', 'UTF-8');
         $element = $dom->importNode($element, true);
-        $this->assertEquals('<endAttemptInteraction id="my-end" class="ending" responseIdentifier="BOOL_RESP" title="End the attempt now!"/>', $dom->saveXML($element));
+        $this->assertEquals('<endAttemptInteraction id="my-end" class="ending" responseIdentifier="BOOL_RESP" title="End the attempt now!" xml:base="/home/jerome"/>', $dom->saveXML($element));
 	}
 	
-	public function testUnmarshall() {
+	public function testUnmarshall()
+    {
         $element = $this->createDOMElement('
-            <endAttemptInteraction id="my-end" class="ending" responseIdentifier="BOOL_RESP" title="End the attempt now!"/>
+            <endAttemptInteraction id="my-end" class="ending" responseIdentifier="BOOL_RESP" title="End the attempt now!" xml:base="/home/jerome"/>
         ');
         
         $component = $this->getMarshallerFactory('2.1.0')->createMarshaller($element)->unmarshall($element);
@@ -27,5 +30,36 @@ class EndAttemptInteractionMarshallerTest extends QtiSmTestCase {
         $this->assertEquals('ending', $component->getClass());
         $this->assertEquals('BOOL_RESP', $component->getResponseIdentifier());
         $this->assertEquals('End the attempt now!', $component->getTitle());
+        $this->assertEquals('/home/jerome', $component->getXmlBase());
+	}
+    
+    /**
+     * @depends testUnmarshall
+     */
+    public function testUnmarshallNoTitle()
+    {
+        $element = $this->createDOMElement('
+            <endAttemptInteraction id="my-end" class="ending" responseIdentifier="BOOL_RESP"/>
+        ');
+        
+        $component = $this->getMarshallerFactory('2.1.0')->createMarshaller($element)->unmarshall($element);
+        $this->assertEquals('', $component->getTitle());
+	}
+    
+    /**
+     * @depends testUnmarshall
+     */
+    public function testUnmarshallNoResponseIdentifier()
+    {
+        $element = $this->createDOMElement('
+            <endAttemptInteraction id="my-end" class="ending"/>
+        ');
+        
+        $this->setExpectedException(
+            'qtism\\data\\storage\\xml\\marshalling\\UnmarshallingException',
+            "The mandatory 'responseIdentifier' attribute is missing from the 'endAttemptInteraction' element."
+        );
+        
+        $this->getMarshallerFactory('2.1.0')->createMarshaller($element)->unmarshall($element);
 	}
 }
