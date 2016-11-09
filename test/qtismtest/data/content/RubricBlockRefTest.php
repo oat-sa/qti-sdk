995c860ec6c365525b9d0884a5553750489d3753
= <jerome@taotesting.com>
2016-11-09 22:55:27 +0100

    Additional unit tests for RubricBlockRef.

---
+++ test/qtismtest/data/content/RubricBlockRefTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace qtismtest\data\content;
+
+use qtismtest\QtiSmTestCase;
+use qtism\data\content\RubricBlockRef;
+
+class RubricBlockRefTest extends QtiSmTestCase
+{
+    public function testCreateWrongIdentifierType()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "The 'identifier' argument must be a valid QTI identifier, '999' given."
+        );
+        $rubricBlockRef = new RubricBlockRef('999', 'href.ref');
+    }
+    
+    public function testCreateWrongHrefType()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "The 'href' argument must be a valid URI, '999' given."
+        );
+        $rubricBlockRef = new RubricBlockRef('ref-1', 999);
+    }
+}
