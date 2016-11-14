0ae1937f0d82ed04467ad6e4d557b3d566e7b366
bugalot <jerome@taotesting.com>
2016-11-14 14:07:56 +0100

    Additional unit tests for Stylesheet.

---
+++ test/qtismtest/data/content/StylesheetTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace qtismtest\data\content;
+
+use qtismtest\QtiSmTestCase;
+use qtism\data\content\Stylesheet;
+
+class StylesheetTest extends QtiSmTestCase
+{
+    public function testCreateWrongHref()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "Href must be a string, 'integer' given."
+        );
+        
+        $stylesheet = new Stylesheet(999);
+    }
+    
+    public function testSetInvalidType()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "Type must be a string, 'integer' given."
+        );
+        
+        $stylesheet = new Stylesheet('style.css');
+        $stylesheet->setType(999);
+    }
+    
+    public function testSetInvalidMedia()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "Media must be a string, 'integer' given."
+        );
+        
+        $stylesheet = new Stylesheet('style.css');
+        $stylesheet->setMedia(999);
+    }
+    
+    public function testSetInvalidTitle()
+    {
+        $this->setExpectedException(
+            '\\InvalidArgumentException',
+            "Title must be a string, 'integer' given."
+        );
+        
+        $stylesheet = new Stylesheet('style.css');
+        $stylesheet->setTitle(999);
+    }
+}
