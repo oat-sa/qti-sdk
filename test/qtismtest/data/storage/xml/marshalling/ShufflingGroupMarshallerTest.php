b3e90bfcd97e663e6aa0171e536e2cc40d1da3ba
bugalot <jerome@taotesting.com>
2016-11-29 16:40:32 +0100

    Additional unit tests for ShufflingGroupMarshaller.

--- test/qtismtest/data/storage/xml/marshalling/ShufflingGroupMarshallerTest.php
+++ test/qtismtest/data/storage/xml/marshalling/ShufflingGroupMarshallerTest.php
@@ -7,22 +7,26 @@ use qtism\data\state\ShufflingGroup;
 use qtism\common\collections\IdentifierCollection;
 use \DOMDocument;
 
-class ShufflingGroupMarshallerTest extends QtiSmTestCase {
-
-	public function testMarshall() {
+class ShufflingGroupMarshallerTest extends QtiSmTestCase
+{
+	public function testMarshall()
+    {
 		
 		$component = new ShufflingGroup(new IdentifierCollection(array('id1', 'id2', 'id3')));
+		$component->setFixedIdentifiers(new IdentifierCollection(array('id2')));
 		$factory = new CompactMarshallerFactory();
 		$marshaller = $factory->createMarshaller($component);
 		$element = $marshaller->marshall($component);
 		
 		$this->assertInstanceOf('\\DOMElement', $element);
 		$this->assertEquals('id1 id2 id3', $element->getAttribute('identifiers'));
+		$this->assertEquals('id2', $element->getAttribute('fixedIdentifiers'));
 	}
 	
-	public function testUnmarshall() {
+	public function testUnmarshall()
+    {
 		$dom = new DOMDocument('1.0', 'UTF-8');
-		$dom->loadXML('<shufflingGroup identifiers="id1 id2 id3"/>');
+		$dom->loadXML('<shufflingGroup identifiers="id1 id2 id3" fixedIdentifiers="id2"/>');
 		$element = $dom->documentElement;
 		
 		$factory = new CompactMarshallerFactory();
@@ -30,7 +34,23 @@ class ShufflingGroupMarshallerTest extends QtiSmTestCase {
 		$component = $marshaller->unmarshall($element);
 		
 		$this->assertInstanceOf('qtism\\data\\state\\ShufflingGroup', $component);
-		$identifiers = $component->getIdentifiers();
-		$this->assertEquals(array('id1', 'id2', 'id3'), $identifiers->getArrayCopy());
+		$this->assertEquals(array('id1', 'id2', 'id3'), $component->getIdentifiers()->getArrayCopy());
+		$this->assertEquals(array('id2'), $component->getFixedIdentifiers()->getArrayCopy());
 	}
+    
+    public function testUnmarshallMissingIdentifiersAttribute()
+    {
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->loadXML('<shufflingGroup fixedIdentifiers="id2"/>');
+        $element = $dom->documentElement;
+        
+        $factory = new CompactMarshallerFactory();
+        
+        $this->setExpectedException(
+            'qtism\\data\\storage\\xml\\marshalling\\UnmarshallingException',
+            "The mandatory attribute 'identifiers' is missing from element 'shufflingGroup'."
+        );
+        
+        $factory->createMarshaller($element)->unmarshall($element);
+    }
 }
