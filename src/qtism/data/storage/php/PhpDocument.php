a9cc55ef6ea45c2ad5725aad23bc331c1e4279a7
bugalot <jerome@taotesting.com>
2016-11-29 11:40:21 +0100

    Additional unit tests for PhpDocument.

--- src/qtism/data/storage/php/PhpDocument.php
+++ src/qtism/data/storage/php/PhpDocument.php
@@ -60,6 +60,7 @@ class PhpDocument extends QtiDocument
      * As PHP-serialized QTI documents contain the necessary information about the QTI version they are
      * refering to, the value of the $version argument will simply be ignored. 
      *
+     * @param string $version (optional) Expected QTI version (default is "2.1").
      * @param QtiComponent $documentComponent The root QtiComponent object to contained by the PhpDocument.
      */
     public function __construct($version = '2.1', QtiComponent $documentComponent = null)
@@ -80,92 +81,91 @@ class PhpDocument extends QtiDocument
 
         // 1st/2nd pass marker.
         $marker = array();
+        
+        // marshalling context.
+        $stream = new MemoryStream();
+        $stream->open();
+        $streamAccess = new PhpStreamAccess($stream);
+        $streamAccess->writeOpeningTag();
+        $ctx = new PhpMarshallingContext($streamAccess);
+        $ctx->setFormatOutput(true);
 
-        try {
-            // marshalling context.
-            $stream = new MemoryStream();
-            $stream->open();
-            $streamAccess = new PhpStreamAccess($stream);
-            $streamAccess->writeOpeningTag();
-            $ctx = new PhpMarshallingContext($streamAccess);
-            $ctx->setFormatOutput(true);
+        while (count($stack) > 0) {
 
-            while (count($stack) > 0) {
+            $component = $stack->pop();
+            $isMarked = in_array($component, $marker, true);
 
-                $component = $stack->pop();
-                $isMarked = in_array($component, $marker, true);
+            if ($isMarked === false && ($component instanceof QtiComponent)) {
+                // -- QtiComponent node, 1st pass.
+                // Mark as explored.
+                array_push($marker, $component);
+                // Ask for a 2nd pass.
+                $stack->push($component);
 
-                if ($isMarked === false && ($component instanceof QtiComponent)) {
-                    // -- QtiComponent node, 1st pass.
-                    // Mark as explored.
-                    array_push($marker, $component);
-                    // Ask for a 2nd pass.
-                    $stack->push($component);
+                // Let's look at the Bean properties and ask for a future exploration.
+                $bean = new Bean($component, false, self::getBaseImplementation($component));
+                $ctorGetters = $bean->getConstructorGetters();
+                $bodyGetters = $bean->getGetters(true);
+                $getters = array_reverse(array_merge($bodyGetters->getArrayCopy(), $ctorGetters->getArrayCopy()));
 
-                    // Let's look at the Bean properties and ask for a future exploration.
-                    $bean = new Bean($component, false, self::getBaseImplementation($component));
-                    $ctorGetters = $bean->getConstructorGetters();
-                    $bodyGetters = $bean->getGetters(true);
-                    $getters = array_reverse(array_merge($bodyGetters->getArrayCopy(), $ctorGetters->getArrayCopy()));
-
-                    foreach ($getters as $getter) {
-                        $stack->push(call_user_func(array($component, $getter->getName())));
-                    }
-                }
-                // Warning!!! Check for Coords Datatype objects. Indeed, it extends AbstractCollection, but must not be considered as it is.
-                elseif ($isMarked === false && ($component instanceof AbstractCollection && !$component instanceof QtiCoords)) {
-                    // AbstractCollection node, 1st pass.
-                    // Mark as explored.
-                    array_push($marker, $component);
-                    // Ask for a 2nd pass.
-                    $stack->push($component);
-
-                    // Explore all values of the collection please!
-                    $values = array_reverse($component->getArrayCopy());
-                    foreach ($values as $val) {
-                        $stack->push($val);
-                    }
-                } elseif ($isMarked === true && $component instanceof QtiComponent) {
-                    // QtiComponent, 2nd pass.
-                    $marshaller = new PhpQtiComponentMarshaller($ctx, $component);
-                    $marshaller->setAsInstanceOf(self::getBaseImplementation($component));
-
-                    if ($component === $this->getDocumentComponent()) {
-                        $marshaller->setVariableName('rootcomponent');
-                    }
-
-                    $marshaller->marshall();
-                } elseif ($component instanceof QtiDatatype) {
-                    // Leaf node QtiDataType.
-                    $marshaller = new PhpQtiDatatypeMarshaller($ctx, $component);
-                    $marshaller->marshall();
-                } elseif ($isMarked === true && $component instanceof AbstractCollection) {
-                    // AbstractCollection, 2nd pass.
-                    $marshaller = new PhpCollectionMarshaller($ctx, $component);
-                    $marshaller->marshall();
-                } elseif (PhpUtils::isScalar($component) === true) {
-                    // Leaf node (QtiDatatype or PHP scalar (including the null value)).
-                    $marshaller = new PhpScalarMarshaller($ctx, $component);
-                    $marshaller->marshall();
-                } elseif (is_array($component) === true) {
-                    // Leaf node array.
-                    $marshaller = new PhpArrayMarshaller($ctx, $component);
-                    $marshaller->marshall();
-                } else {
-                    $msg = "Datatype '" . gettype($component) . "' cannot be handled by the PhpDocument::save() method.";
-                    throw new PhpStorageException($msg);
+                foreach ($getters as $getter) {
+                    $stack->push(call_user_func(array($component, $getter->getName())));
                 }
             }
+            // Warning!!! Check for Coords Datatype objects. Indeed, it extends AbstractCollection, but must not be considered as it is.
+            elseif ($isMarked === false && ($component instanceof AbstractCollection && !$component instanceof QtiCoords)) {
+                // AbstractCollection node, 1st pass.
+                // Mark as explored.
+                array_push($marker, $component);
+                // Ask for a 2nd pass.
+                $stack->push($component);
 
-            $exists = file_exists($url);
-            $written = file_put_contents($url, $stream->getBinary());
-            
-            if ($written !== false && $exists === true && function_exists('opcache_invalidate') === true) {
-                opcache_invalidate($url, true);
+                // Explore all values of the collection please!
+                $values = array_reverse($component->getArrayCopy());
+                foreach ($values as $val) {
+                    $stack->push($val);
+                }
+            } elseif ($isMarked === true && $component instanceof QtiComponent) {
+                // QtiComponent, 2nd pass.
+                $marshaller = new PhpQtiComponentMarshaller($ctx, $component);
+                $marshaller->setAsInstanceOf(self::getBaseImplementation($component));
+
+                if ($component === $this->getDocumentComponent()) {
+                    $marshaller->setVariableName('rootcomponent');
+                }
+
+                $marshaller->marshall();
+            } elseif ($component instanceof QtiDatatype) {
+                // Leaf node QtiDataType.
+                $marshaller = new PhpQtiDatatypeMarshaller($ctx, $component);
+                $marshaller->marshall();
+            } elseif ($isMarked === true && $component instanceof AbstractCollection) {
+                // AbstractCollection, 2nd pass.
+                $marshaller = new PhpCollectionMarshaller($ctx, $component);
+                $marshaller->marshall();
+            } elseif (PhpUtils::isScalar($component) === true) {
+                // Leaf node (QtiDatatype or PHP scalar (including the null value)).
+                $marshaller = new PhpScalarMarshaller($ctx, $component);
+                $marshaller->marshall();
+            } elseif (is_array($component) === true) {
+                // Leaf node array.
+                $marshaller = new PhpArrayMarshaller($ctx, $component);
+                $marshaller->marshall();
+            } else {
+                $msg = "Datatype '" . gettype($component) . "' cannot be handled by the PhpDocument::save() method.";
+                throw new PhpStorageException($msg);
             }
-        } catch (StreamAccessException $e) {
-            $msg = "An error occured while writing the PHP source code stream.";
-            throw new PhpStorageException($msg, PhpStorageException::WRITE, $e);
+        }
+
+        $exists = file_exists($url);
+        $written = @file_put_contents($url, $stream->getBinary());
+        
+        if ($written === false) {
+            throw new PhpStorageException("File located at '${url}' could not be written.");
+        }
+        
+        if ($written !== false && $exists === true && function_exists('opcache_invalidate') === true) {
+            opcache_invalidate($url, true);
         }
     }
 
@@ -183,6 +183,7 @@ class PhpDocument extends QtiDocument
         }
 
         try {
+            // Will return $rootcomponent.
             require $url;
             $this->setDocumentComponent($rootcomponent);
             $this->setUrl($url);
