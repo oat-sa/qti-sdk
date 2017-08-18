ebb5dc611f245f3eb49c21caf61810a8667f50c7
bugalot <jerome@taotesting.com>
2017-08-18 16:41:45 +0200

    No exception on selection.

--- src/qtism/runtime/tests/BasicSelection.php
+++ src/qtism/runtime/tests/BasicSelection.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -58,10 +58,10 @@ class BasicSelection extends AbstractSelection
             $withReplacement = $selection->isWithReplacement();
 
             if ($select > $childCount && $withReplacement !== true) {
-                $assessmentSectionIdentifier = $assessmentSection->getIdentifier();
-                $msg = "The number of children to select (${select}) cannot exceed the number ";
-                $msg.= "of child elements defined (${childCount}) in assessmentSection '${assessmentSectionIdentifier}'.";
-                throw new SelectionException($msg, SelectionException::LOGIC_ERROR);
+                // In case of the requested selection is greather than the actual
+                // selectable elements, the requested selection is lowered down
+                // to number of children elements.
+                $select = $childCount;
             }
 
             // Map used to count the amount of selection by Route.
