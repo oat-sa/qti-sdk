cee4cdafd3f1c80261ec8baf453e69abcec7ca7d
bugalot <jerome@taotesting.com>
2017-08-21 14:28:12 +0200

    No more SelectionException.

--- test/qtism/runtime/tests/AssessmentTestSessionTest.php
+++ test/qtism/runtime/tests/AssessmentTestSessionTest.php
@@ -1105,60 +1105,6 @@ class AssessmentTestSessionTest extends QtiSmTestCase {
 	    );
 	}
 	
-	public function testSelectionAndOrdering() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/selection_and_ordering_with_replacement.xml');
-	    
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(50, $assessmentTestSession->getRouteCount());
-	}
-	
-	public function testOrderingBasic() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/ordering_basic.xml');
-
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(3, $assessmentTestSession->getRouteCount());
-	}
-	
-	public function testOrderingBasicFixed() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/ordering_basic_fixed.xml');
-	    
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(5, $assessmentTestSession->getRouteCount());
-	}
-    
-	public function testOrderingVisible() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/ordering_visible.xml');
-	     
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(9, $assessmentTestSession->getRouteCount());
-	}
-	
-	public function testOrderingInvisibleDontKeepTogether() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/ordering_invisible_dont_keep_together.xml');
-	
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(12, $assessmentTestSession->getRouteCount());
-	}
-	
-	public function testOrderingInvisibleKeepTogether() {
-	    $doc = new XmlCompactDocument();
-	    $doc->load(self::samplesDir() . 'custom/runtime/ordering_invisible_keep_together.xml');
-	
-	    $sessionManager = new SessionManager();
-	    $assessmentTestSession = $sessionManager->createAssessmentTestSession($doc->getDocumentComponent());
-	    $this->assertEquals(12, $assessmentTestSession->getRouteCount());
-	}
-	
 	public function testRouteItemAssessmentSections() {
 	    $doc = new XmlCompactDocument();
 	    $doc->load(self::samplesDir() . 'custom/runtime/routeitem_assessmentsections.xml');
