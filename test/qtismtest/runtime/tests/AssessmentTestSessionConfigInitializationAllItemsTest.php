dfcbb0143fa44d93f0814030fca2d1fba3da2cd9
Eugene Golubev <emgolubev@gmail.com>
2019-10-24 20:11:23 +0300

    added opposite test cases

--- test/qtismtest/runtime/tests/AssessmentTestSessionConfigInitializationAllItemsTest.php
+++ test/qtismtest/runtime/tests/AssessmentTestSessionConfigInitializationAllItemsTest.php
@@ -1,31 +1,30 @@
 <?php
 namespace qtismtest\runtime\tests;
 
+use qtism\data\storage\xml\XmlStorageException;
 use qtismtest\QtiSmAssessmentTestSessionTestCase;
 use qtism\common\datatypes\files\FileSystemFileManager;
 use qtism\runtime\tests\SessionManager;
 use qtism\runtime\tests\AssessmentTestSession;
 use qtism\data\storage\xml\XmlCompactDocument;
 
-class AssessmentTestSessionConfigInitializationAllItemsTest extends QtiSmAssessmentTestSessionTestCase {
+class AssessmentTestSessionConfigInitializationAllItemsTest extends QtiSmAssessmentTestSessionTestCase
+{
 
     /**
      * @dataProvider getBranchingTestCases
+     * @param int $config
      * @param string $path
      * @param int $expectedCountOfItems
-     * @throws \qtism\data\storage\xml\XmlStorageException
+     * @throws XmlStorageException
      */
-    public function testConfigInitializationAllItemsWorksProperly($path, $expectedCountOfItems)
+    public function testConfigInitializationAllItemsWorksProperly($config, $path, $expectedCountOfItems = 1)
     {
         $doc = new XmlCompactDocument('2.1');
         $doc->load($path);
-        
+
         $manager = new SessionManager(new FileSystemFileManager());
-        $testSession = $manager->createAssessmentTestSession(
-            $doc->getDocumentComponent(),
-            null,
-            AssessmentTestSession::INITIALIZE_ALL_ITEMS
-        );
+        $testSession = $manager->createAssessmentTestSession($doc->getDocumentComponent(), null, $config);
 
         $testSession->beginTestSession();
 
@@ -38,9 +37,14 @@ class AssessmentTestSessionConfigInitializationAllItemsTest extends QtiSmAssessm
     public function getBranchingTestCases()
     {
         return array(
-            array(self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_linear.xml', 4),
-            array(self::samplesDir() . 'custom/runtime/branchings/branchings_multiple_occurences.xml', 5),
-            array(self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_nonlinear.xml', 4),
+            // config INITIALIZE_ALL_ITEMS is enabled
+            array(AssessmentTestSession::INITIALIZE_ALL_ITEMS, self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_linear.xml', 4),
+            array(AssessmentTestSession::INITIALIZE_ALL_ITEMS, self::samplesDir() . 'custom/runtime/branchings/branchings_multiple_occurences.xml', 5),
+            array(AssessmentTestSession::INITIALIZE_ALL_ITEMS, self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_nonlinear.xml', 4),
+            // config INITIALIZE_ALL_ITEMS is disabled
+            array(0, self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_linear.xml'),
+            array(0, self::samplesDir() . 'custom/runtime/branchings/branchings_multiple_occurences.xml'),
+            array(0, self::samplesDir() . 'custom/runtime/branchings/branchings_single_section_nonlinear.xml'),
         );
     }
 }
