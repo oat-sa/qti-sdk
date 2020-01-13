716c69437009ba6cf03ec3c6eeb52ab0592ee1f2
Julien Sébire <julien@taotesting.com>
2020-01-13 15:40:12 +0100

    Fixed copyright year.

--- src/qtism/common/beans/Bean.php
+++ src/qtism/common/beans/Bean.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
