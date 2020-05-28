2fa46889b9e9701782c62c07bccb77e0ec1d7c77
Julien Sébire <julien@taotesting.com>
2020-05-28 15:51:07 +0200

    Reverted code style.

--- test/qtismtest/common/datatypes/PairTest.php
+++ test/qtismtest/common/datatypes/PairTest.php
@@ -1,18 +1,16 @@
 <?php
-
 namespace qtismtest\common\datatypes;
 
-use qtism\common\datatypes\QtiPair;
 use qtismtest\QtiSmTestCase;
+use qtism\common\datatypes\QtiPair;
 
-class PairTest extends QtiSmTestCase
-{
-    public function testEquality()
-    {
-        $p1 = new QtiPair('A', 'B');
-        $p2 = new QtiPair('A', 'B');
-        $p3 = new QtiPair('C', 'D');
-        $p4 = new QtiPair('D', 'C');
+class PairTest extends QtiSmTestCase {
+
+	public function testEquality() {
+		$p1 = new QtiPair('A', 'B');
+		$p2 = new QtiPair('A', 'B');
+		$p3 = new QtiPair('C', 'D');
+		$p4 = new QtiPair('D', 'C');
 
         $this->assertEquals('A B', $p1->getValue());
         $this->assertEquals('A B', $p2->getValue());
@@ -20,23 +18,21 @@ class PairTest extends QtiSmTestCase
         $this->assertEquals('D C', $p4->getValue());
 
         $this->assertTrue($p1->equals($p2));
-        $this->assertTrue($p2->equals($p1));
-        $this->assertFalse($p1->equals($p3));
-        $this->assertFalse($p3->equals($p1));
-        $this->assertFalse($p3->equals(1337));
-        $this->assertTrue($p3->equals($p3));
-        $this->assertTrue($p4->equals($p3));
-    }
-
-    public function testInvalidFirstIdentifier()
-    {
-        $this->setExpectedException('\\InvalidArgumentException');
-        $pair = new QtiPair('_33', '33tt');
-    }
-
-    public function testInvalidSecondIdentifier()
-    {
-        $this->setExpectedException('\\InvalidArgumentException');
-        $pair = new QtiPair('33tt', '_33');
-    }
+		$this->assertTrue($p2->equals($p1));
+		$this->assertFalse($p1->equals($p3));
+		$this->assertFalse($p3->equals($p1));
+		$this->assertFalse($p3->equals(1337));
+		$this->assertTrue($p3->equals($p3));
+		$this->assertTrue($p4->equals($p3));
+	}
+	
+	public function testInvalidFirstIdentifier() {
+	    $this->setExpectedException('\\InvalidArgumentException');
+	    $pair = new QtiPair('_33', '33tt');
+	}
+	
+	public function testInvalidSecondIdentifier() {
+	    $this->setExpectedException('\\InvalidArgumentException');
+	    $pair = new QtiPair('33tt', '_33');
+	}
 }
--- test/qtismtest/common/datatypes/PointTest.php
+++ test/qtismtest/common/datatypes/PointTest.php
@@ -1,27 +1,25 @@
 <?php
-
 namespace qtismtest\common\datatypes;
 
-use qtism\common\datatypes\QtiPoint;
 use qtismtest\QtiSmTestCase;
+use qtism\common\datatypes\QtiPoint;
 
-class PointTest extends QtiSmTestCase
-{
-    public function testEquality()
-    {
-        $p1 = new QtiPoint(10, 10);
-        $p2 = new QtiPoint(10, 10);
-        $p3 = new QtiPoint(100, 100);
+class PointTest extends QtiSmTestCase {
+
+	public function testEquality() {
+		$p1 = new QtiPoint(10, 10);
+		$p2 = new QtiPoint(10, 10);
+		$p3 = new QtiPoint(100, 100);
 
         $this->assertEquals('10 10', $p1->getValue());
         $this->assertEquals('10 10', $p2->getValue());
         $this->assertEquals('100 100', $p3->getValue());
 
-        $this->assertTrue($p1->equals($p2));
-        $this->assertTrue($p2->equals($p1));
-        $this->assertFalse($p1->equals($p3));
-        $this->assertFalse($p3->equals($p1));
-        $this->assertFalse($p3->equals(1337));
-        $this->assertTrue($p3->equals($p3));
-    }
+		$this->assertTrue($p1->equals($p2));
+		$this->assertTrue($p2->equals($p1));
+		$this->assertFalse($p1->equals($p3));
+		$this->assertFalse($p3->equals($p1));
+		$this->assertFalse($p3->equals(1337));
+		$this->assertTrue($p3->equals($p3));
+	}
 }
