dd699d3630ba1c8676337c9be1995a00f035dbf5
Julien Sébire <julien@taotesting.com>
2020-08-14 15:26:59 +0200

    Allowed <a> in prompt for QTI 2.2.

--- test/qtismtest/data/storage/xml/marshalling/PromptMarshallerTest.php
+++ test/qtismtest/data/storage/xml/marshalling/PromptMarshallerTest.php
@@ -4,8 +4,11 @@ namespace qtismtest\data\storage\xml\marshalling;
 
 use DOMDocument;
 use qtism\data\content\FlowStaticCollection;
+use qtism\data\content\InlineCollection;
 use qtism\data\content\interactions\Prompt;
 use qtism\data\content\TextRun;
+use qtism\data\content\xhtml\A;
+use qtism\data\storage\xml\marshalling\UnmarshallingException;
 use qtismtest\QtiSmTestCase;
 
 class PromptMarshallerTest extends QtiSmTestCase
@@ -37,5 +40,39 @@ class PromptMarshallerTest extends QtiSmTestCase
         $content = $component->getContent();
         $this->assertEquals(1, count($content));
         $this->assertEquals('This is a prompt', $content[0]->getContent());
+    }
+
+    public function testUnmarshallPromptWithAnchorInQti21ThrowsException()
+    {
+        $element = $this->createDOMElement('<prompt id="my-prompt" class="qti-prompt">This is an anchor: <a href="#">anchor text</a></prompt>');
+
+        $marshaller = $this->getMarshallerFactory('2.1')->createMarshaller($element);
+        $this->expectException(UnmarshallingException::class);
+        $this->expectExceptionMessage("A 'prompt' cannot contain 'a' elements.");
+        $marshaller->unmarshall($element);
+    }
+
+    public function testUnmarshallPromptWithAnchorInQti22Works()
+    {
+        $element = $this->createDOMElement('<prompt id="my-prompt" class="qti-prompt">This is an anchor: <a href="#">anchor text</a></prompt>');
+
+        $marshaller = $this->getMarshallerFactory('2.2')->createMarshaller($element);
+        $component = $marshaller->unmarshall($element);
+
+        $this->assertInstanceOf(Prompt::class, $component);
+        $this->assertEquals('my-prompt', $component->getId());
+        $this->assertEquals('qti-prompt', $component->getClass());
+
+        $content = $component->getContent();
+        $this->assertCount(2, $content);
+        $this->assertEquals('This is an anchor: ', $content[0]->getContent());
+
+        $this->assertInstanceOf(A::class, $content[1]);
+        $this->assertEquals('#', $content[1]->getHref());
+
+        $this->assertInstanceOf(InlineCollection::class, $content[1]->getContent());
+        $linkContent = $content[1]->getContent()[0];
+        $this->assertInstanceOf(TextRun::class, $linkContent);
+        $this->assertEquals('anchor text', $linkContent->getContent());
     }
 }
