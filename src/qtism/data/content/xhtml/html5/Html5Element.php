b4dfce34cb45a4debaa9cb63b0b7fb10379e3ac8
Julien Sébire <julien@taotesting.com>
2020-10-07 09:19:40 +0200

    Added Html5 Element abstract class.

---
+++ src/qtism/data/content/xhtml/html5/Html5Element.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Copyright (c) 2013-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ * @license GPLv2
+ */
+
+namespace qtism\data\content\xhtml\html5;
+
+use InvalidArgumentException;
+use qtism\data\content\BodyElement;
+
+/**
+ * The base Html 5 element.
+ */
+abstract class Html5Element extends BodyElement
+{
+    /**
+     * Title of the Html5 element.
+     * @var string
+     */
+    private $title = '';
+
+    /**
+     * Role of the Html5 element.
+     * @var int
+     */
+    private $role;
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        if (!is_string($title)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'The "title" argument must be a string, "%s" given.',
+                    gettype($title)
+                )
+            );
+        }
+
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Sets the role of the html5 element.
+     * @param int $role One of the Role constants.
+     */
+    public function setRole($role)
+    {
+        if (!in_array($role, Role::asArray(), true)) {
+            $given = is_int($role)
+                ? $role
+                : gettype($role);
+
+            throw new InvalidArgumentException(
+                sprintf(
+                    'The "role" argument must be a value from the Role enumeration, "%s" given.',
+                    $given
+                )
+            );
+        }
+
+        $this->role = $role;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+}
---
+++ test/qtismtest/data/content/xhtml/html5/Html5ElementTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace qtismtest\data\content\xhtml\html5;
+
+use InvalidArgumentException;
+use qtism\data\content\xhtml\html5\Html5Element;
+use qtism\data\content\xhtml\html5\Role;
+use qtismtest\QtiSmTestCase;
+
+class Html5ElementTest extends QtiSmTestCase
+{
+    public function testCreateWithValidTitle()
+    {
+        $title = 'a title';
+            
+        $subject = new fakeHtml5Element();
+
+        $subject->setTitle($title);
+        $this->assertEquals($title, $subject->getTitle());
+    }
+
+    public function testCreateWithNonStringTitle()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "title" argument must be a string, "integer" given.');
+
+        (new FakeHtml5Element())->setTitle(12);
+    }
+
+    /**
+     * @dataProvider rolesToTest
+     * @param int $role
+     */
+    public function testCreateWithValidRole(int $role)
+    {
+        $subject = new fakeHtml5Element();
+
+        $subject->setRole($role);
+        $this->assertEquals($role, $subject->getRole());
+    }
+
+    public function rolesToTest(): array
+    {
+        return [
+            [Role::ARTICLE],
+            [Role::BUTTON],
+            [Role::CHECKBOX],
+            [Role::COLUMN_HEADER],
+            [Role::COMPLEMENTARY],
+            [Role::CONTENT_INFO],
+            [Role::DEFINITION],
+            [Role::DIRECTORY],
+            [Role::DOCUMENT],
+            [Role::GRID_CELL],
+            [Role::GROUP],
+            [Role::HEADING],
+            [Role::IMG],
+            [Role::LINK],
+            [Role::LIST],
+            [Role::LIST_BOX],
+            [Role::LIST_ITEM],
+            [Role::LOG],
+            [Role::MATH],
+            [Role::NOTE],
+            [Role::OPTION],
+            [Role::PRESENTATION],
+            [Role::RADIO],
+            [Role::RADIO_GROUP],
+            [Role::REGION],
+            [Role::ROW],
+            [Role::ROW_GROUP],
+            [Role::ROW_HEADER],
+            [Role::SEPARATOR],
+            [Role::SLIDER],
+            [Role::SPIN_BUTTON],
+            [Role::STATUS],
+            [Role::TAB],
+            [Role::TAB_LIST],
+            [Role::TAB_PANEL],
+            [Role::TEXT_BOX],
+            [Role::TIMER],
+            [Role::TOOLBAR],
+        ];
+    }
+
+    public function testCreateWithNonIntegerRole()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "role" argument must be a value from the Role enumeration, "string" given.');
+
+        (new FakeHtml5Element())->setRole('foo');
+    }
+
+    public function testCreateWithInvalidRole()
+    {
+        $wrongRole = 1012;
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "role" argument must be a value from the Role enumeration, "' . $wrongRole . '" given.');
+
+        (new FakeHtml5Element())->setRole(1012);
+    }
+}
+
+class FakeHtml5Element extends Html5Element
+{
+    public function getQtiClassName()
+    {
+    }
+
+    public function getComponents()
+    {
+    }
+}
