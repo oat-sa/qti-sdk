497cc1319924b444e566b0e30627e7526f1406c7
Julien Sébire <julien@taotesting.com>
2021-01-14 15:42:25 +0100

    Replaced usage of gettype with is_* functions.

--- src/qtism/runtime/tests/AssessmentTestSession.php
+++ src/qtism/runtime/tests/AssessmentTestSession.php
@@ -363,7 +363,7 @@ class AssessmentTestSession extends State
      */
     public function setSessionId($sessionId)
     {
-        if (gettype($sessionId) === 'string') {
+        if (is_string($sessionId)) {
             if (empty($sessionId) === false) {
                 $this->sessionId = $sessionId;
             } else {
@@ -1304,7 +1304,7 @@ class AssessmentTestSession extends State
      */
     public function whichLastOccurenceUpdate($assessmentItemRef)
     {
-        if (gettype($assessmentItemRef) === 'string') {
+        if (is_string($assessmentItemRef)) {
             $assessmentItemRefs = $this->getAssessmentItemRefs();
             if (isset($assessmentItemRefs[$assessmentItemRef])) {
                 $assessmentItemRef = $assessmentItemRefs[$assessmentItemRef];
@@ -1700,7 +1700,7 @@ class AssessmentTestSession extends State
      */
     public function getWeight($identifier)
     {
-        if (gettype($identifier) === 'string') {
+        if (is_string($identifier)) {
             try {
                 $identifier = new VariableIdentifier($identifier);
                 if ($identifier->hasSequenceNumber() === true) {
