82965358693982a9edc3f0bba133ef80d01888c3
Julien Sébire <julien@taotesting.com>
2021-02-20 23:25:13 +0100

    Added compatibility pack for Match operator in PHP 7.*.
    (cherry picked from commit 6f765e9e39e5da437135f12a5a9cdc075e3ffb5f)

---
+++ test/qtismtest/data/expressions/operators/MatchTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace qtismtest\data\expressions\operators;
+
+use PHPUnit\Framework\TestCase;
+use qtism\data\expressions\ExpressionCollection;
+use qtism\data\expressions\operators\MatchOperator;
+
+/**
+ * This class provides test for the backward compatibility Match operator in
+ * PHP 7.*.
+ * In PHP 8.*, match is a reserved word, the Match operator class has been
+ * renamed MacthOperator but compact tests contain generated PHP code which
+ * contains references to the Match class. This class makes sure these compact
+ * tests still run in PHP 7.*. When run on PHP 8.0, the compact tests have to
+ * be updated either by re-publishing the test or by running a script to update
+ * the generated PHP code.
+ */
+class MatchTest extends TestCase
+{
+    public function testClassCreation()
+    {
+        if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+            $expression = $this->createMock(ExpressionCollection::class);
+            $matchOperator = new \qtism\data\expressions\operators\Match($expression);
+            $this::assertInstanceOf(MatchOperator::class, $matchOperator);
+        } else {
+            $this::assertTrue(true);
+        }
+    }
+}
