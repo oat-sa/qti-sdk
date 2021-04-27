19a4626bbe44af46df1eab20a710fe0d66f6e357
Julien Sébire <julien@taotesting.com>
2021-04-27 02:16:04 +0200

    Added % support in Img and ImgMarshaller.

--- test/qtismtest/data/content/xhtml/ImgTest.php
+++ test/qtismtest/data/content/xhtml/ImgTest.php
@@ -19,21 +19,85 @@ class ImgTest extends QtiSmTestCase
         new Img('999.png', 999);
     }
 
-    public function testSetHeightWrongFormat()
+    /**
+     * @dataProvider lengthsToTest
+     * @param mixed $width
+     * @param string|null $expected
+     */
+    public function testSetWidth($width, ?string $expected): void
     {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("The 'height' argument must be a valid XHTML length value, '999xp' given.");
-
-        $img = new Img('999.png', '999');
-        $img->setHeight('999xp');
+        $object = new Img('999.png', '999');
+        $object->setWidth($width);
+        self::assertEquals($expected, $object->getWidth());
     }
 
-    public function testSetWidthWrongFormat()
+    /**
+     * @dataProvider lengthsToTest
+     * @param mixed $height
+     * @param string|null $expected
+     */
+    public function testSetHeight($height, ?string $expected): void
+    {
+        $object = new Img('999.png', '999');
+        $object->setHeight($height);
+        self::assertEquals($expected, $object->getHeight());
+    }
+
+    public function lengthsToTest(): array
+    {
+        return [
+            ['10%', '10%'],
+            [10, '10'],
+            ['10', '10'],
+            [null, null],
+            [-1, null],
+        ];
+    }
+
+    /**
+     * @dataProvider wrongLengthsToTest
+     * @param mixed $height
+     * @param string|null $message
+     */
+    public function testSetHeightWrongFormat($height, string $message = null): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("The 'width' argument must be a valid XHTML length value, '999xp' given.");
+        $this->expectExceptionMessage('The "height" argument must be a positive integer with optional percent sign, "' . $message ?? $height . '" given.');
 
         $img = new Img('999.png', '999');
-        $img->setWidth('999xp');
+        $img->setHeight($height);
+    }
+
+    /**
+     * @dataProvider wrongLengthsToTest
+     * @param mixed $width
+     * @param string|null $message
+     */
+    public function testSetWidthWrongFormat($width, string $message = null): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "width" argument must be a positive integer with optional percent sign, "' . $message ?? $width . '" given.');
+
+        $img = new Img('999.png', '999');
+        $img->setWidth($width);
+    }
+
+    public function wrongLengthsToTest(): array
+    {
+        return [
+            [999.999],
+            [-10],
+            ['10px'],
+            [[], 'array'],
+            [
+                new class() {
+                    public function __toString(): string
+                    {
+                        return '10%';
+                    }
+                },
+                'object',
+            ],
+        ];
     }
 }
