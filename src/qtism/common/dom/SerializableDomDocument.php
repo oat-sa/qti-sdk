3458457c0fb2738c9ba1ce8c07d7ae88b11282d3
Vitalii Shtykhno <kilatib@gmail.com>
2022-07-13 17:15:08 +0200

    fix: add hack to allow possibility of use seriazation on PHP 8.1

--- src/qtism/common/dom/SerializableDomDocument.php
+++ src/qtism/common/dom/SerializableDomDocument.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2017-2022 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -24,28 +24,71 @@
 namespace qtism\common\dom;
 
 use DOMDocument;
+use Exception;
 
 /**
  * Serializable DOM Document
  *
  * This class is a PHP Serializable DOMDocument implementation.
+ * @serializable
  */
-class SerializableDomDocument extends DOMDocument
+class SerializableDomDocument
 {
-    private $xmlData;
+    private DOMDocument $dom;
 
-    public function __serialize()
+    public function __construct(string $version = '1.0', string $encoding = '')
     {
-        $this->xmlData = $this->saveXML();
+        $this->dom = new DOMDocument($version, $encoding);
+    }
+
+    public function __serialize(): array
+    {
         return [
-            'xmlData' => $this->xmlData
+            'version'  => $this->dom->xmlVersion,
+            'encoding' => $this->dom->encoding,
+            'xmlData'  => (string)$this->__toString()
         ];
     }
 
-    public function __unserialize(array $data)
+    public function __unserialize(array $data): void
     {
-        return [
-            'xmlData' => $this->xmlData
-        ];
+        $this->dom = new DOMDocument($data['version'], $data['encoding']);
+        $this->dom->loadXML($data['xmlData']);
+    }
+
+    public function __toString(): string
+    {
+        return $this->dom->saveXML();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function __call($name, $arguments)
+    {
+        if (!method_exists($this->dom, $name)) {
+            throw new Exception(__CLASS__ . 'has no method::' . $name);
+        }
+
+        return call_user_func_array([$this->dom, $name], $arguments);
+    }
+
+    public function __get($name)
+    {
+        if (!property_exists($this->dom, $name)) {
+            throw new Exception(__CLASS__ . 'has no property::' . $name);
+        }
+
+        return $this->dom->$name ?? null;
+    }
+
+    public function __set($name, $value)
+    {
+        if (!property_exists($this->dom, $name)) {
+            throw new Exception(__CLASS__ . 'has no property::' . $name);
+        }
+        $this->dom->$name = $value;
+
+        return $this->dom;
     }
 }
