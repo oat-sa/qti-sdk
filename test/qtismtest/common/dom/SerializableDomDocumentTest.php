d6471b7433d7574b1827a8568770241a4b92da4a
Vitalii Shtykhno <kilatib@gmail.com>
2022-07-18 13:50:02 +0200

    tests: add test to magic methods

--- test/qtismtest/common/dom/SerializableDomDocumentTest.php
+++ test/qtismtest/common/dom/SerializableDomDocumentTest.php
@@ -12,12 +12,66 @@ class SerializableDomDocumentTest extends QtiSmTestCase
 {
     public function testSerialization()
     {
-        $dom = new SerializableDomDocument('1.0', 'UTF-8');
-        $dom->load(self::samplesDir() . 'ims/items/2_2_1/choice.xml');
-
-        $ser = serialize($dom);
+        $ser = serialize($this->getSerializableDomDocument());
         $dom = unserialize($ser);
 
         $this::assertEquals('http://www.imsglobal.org/xsd/imsqti_v2p2', $dom->documentElement->namespaceURI);
+    }
+
+
+    public function testAccessingProperty()
+    {
+        $xmlVersion = '1.0';
+        $dom = $this->getSerializableDomDocument($xmlVersion);
+
+        $this->assertNotEmpty($dom->xmlVersion);
+        $this->assertEquals($xmlVersion, $dom->xmlVersion);
+    }
+
+    public function testAccessingInexistentProperty()
+    {
+        $dom = $this->getSerializableDomDocument();
+        $property = 'test';
+
+        $this->expectError();
+        $this->expectErrorMessage(
+            sprintf('Undefined property: %s::%s', SerializableDomDocument::class, $property)
+        );
+
+        $dom->$property;
+    }
+
+    public function testSettingVirtualPropertyToDom()
+    {
+        $xmlVersion = '1.0';
+        $dom = $this->getSerializableDomDocument($xmlVersion);
+
+        $this->assertEquals($xmlVersion, $dom->xmlVersion);
+
+        $dom->xmlVersion = '1.1';
+        $this->assertEquals('1.1', $dom->xmlVersion);
+    }
+
+    public function testCheckingIfPropertyExists()
+    {
+        $dom = $this->getSerializableDomDocument();
+
+        $this->assertTrue(isset($dom->xmlVersion));
+    }
+
+    public function testCallingVirtualMethods()
+    {
+        $dom = $this->getSerializableDomDocument();
+
+        $this->assertNotEmpty($dom->saveXML());
+        $this->assertNotEmpty((string)$dom);
+    }
+
+    private function getSerializableDomDocument(string $version = '1.0', string $encoding = 'UTF-8'): SerializableDomDocument
+    {
+        $dom = new SerializableDomDocument($version, $encoding);
+        $dom->load(self::samplesDir() . 'ims/items/2_2_1/choice.xml');
+
+        return $dom;
     }
 }
