31c035a09a9456376def896124b6396e35f3040a
poyuk <kyril.poyu@taotesting.com>
2023-03-07 13:18:19 +0100

    feat: add session qti storage used session serializer and storage driver abstraction

---
+++ src/qtism/runtime/storage/serializable/QtiSerializableStorage.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2023 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace qtism\runtime\storage\serializable;
+
+use Exception;
+use qtism\common\datatypes\files\FileManagerException;
+use qtism\data\AssessmentTest;
+use qtism\runtime\storage\common\AbstractStorage;
+use qtism\runtime\storage\common\StorageException;
+use qtism\runtime\storage\driver\exception\DriverDeletionException;
+use qtism\runtime\storage\driver\StorageDriverInterface;
+use qtism\runtime\tests\AbstractSessionManager;
+use qtism\runtime\tests\AssessmentTestSession;
+
+class QtiSerializableStorage extends AbstractStorage
+{
+    private SerializerInterface $serializer;
+    private StorageDriverInterface $storageDriver;
+
+    public function __construct(
+        AbstractSessionManager $manager,
+        AssessmentTest $test,
+        SerializerInterface $serializer,
+        StorageDriverInterface $storageDriver
+    ) {
+        $this->serializer = $serializer;
+        $this->storageDriver = $storageDriver;
+        parent::__construct($manager, $test);
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function instantiate($config = 0, $sessionId = ''): AssessmentTestSession
+    {
+        // If not provided, generate a session ID.
+        if (empty($sessionId)) {
+            $sessionId = uniqid('qtism', true);
+        }
+
+        try {
+            $session = $this->getManager()->createAssessmentTestSession($this->getAssessmentTest(), null, $config);
+            $session->setSessionId($sessionId);
+
+            return $session;
+        } catch (Exception $e) {
+            $msg = 'An error occurred while instantiating the given AssessmentTest.';
+            throw new StorageException($msg, StorageException::INSTANTIATION, $e);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function persist(AssessmentTestSession $assessmentTestSession)
+    {
+        $serializedSession = $this->serializer->encode($assessmentTestSession);
+        $this->storageDriver->write($assessmentTestSession->getSessionId(), $serializedSession);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function retrieve($sessionId)
+    {
+        $serializedSession = $this->storageDriver->read($sessionId);
+        return $this->serializer->decode($serializedSession);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function exists($sessionId): bool
+    {
+        return $this->storageDriver->exists($sessionId);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(AssessmentTestSession $assessmentTestSession): bool
+    {
+        $fileManager = $this->getManager()->getFileManager();
+        foreach ($assessmentTestSession->getFiles() as $file) {
+            try {
+                $fileManager->delete($file);
+            } catch (FileManagerException $e) {
+                throw new StorageException(
+                    "An unexpected error occurred while deleting file '" . $file->getIdentifier() . "' bound to Assessment Test Session '" . $assessmentTestSession->getSessionId() . "'.",
+                    StorageException::DELETION,
+                    $e
+                );
+            }
+        }
+
+        try {
+            $this->storageDriver->delete($assessmentTestSession->getSessionId());
+            return true;
+        } catch (DriverDeletionException) {
+            return false;
+        }
+    }
+}
