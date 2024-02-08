eda34981b234a1fa9d445d7de50fbe6dd5faeb7e
poyuk <kyril.poyu@taotesting.com>
2024-02-08 12:03:37 +0100

    feat: adjust qti v3 versions classes byt referencing on new schemas, and proper namespaces

--- src/qtism/data/storage/xml/versions/ResultVersion30.php
+++ src/qtism/data/storage/xml/versions/ResultVersion30.php
@@ -15,28 +15,23 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
- * @author Julien Sébire <julien@taotesting.com>
- * @license GPLv2
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA;
  */
+
+declare(strict_types=1);
 
 namespace qtism\data\storage\xml\versions;
 
+use qtism\data\storage\xml\marshalling\Qti22MarshallerFactory;
 use qtism\data\storage\xml\marshalling\Qti30MarshallerFactory;
 
-/**
- * QTI version 3.0.0
- *
- * This class is a draft since QTI 3.0 hasn't been published yet.
- */
-class QtiVersion300 extends QtiVersion
+class ResultVersion30 extends ResultVersion
 {
-    public const XMLNS = 'http://www.imsglobal.org/xsd/imsqtiasi_v3p0';
+    public const XMLNS = 'http://www.imsglobal.org/xsd/imsqti_result_v3p0';
 
-    public const XSD = 'https://purl.imsglobal.org/spec/qti/v3p0/schema/xsd/imsqti_asiv3p0_v1p0.xsd';
+    public const XSD = 'https://purl.imsglobal.org/spec/qti/v3p0/schema/xsd/imsqti_resultv3p0_v1p0.xsd';
 
-    public const LOCAL_XSD = 'qtiv3p0/imsqti_asiv3p0_v1p0.xsd';
+    public const LOCAL_XSD = 'qtiv3p0/imsqti_resultv3p0_v1p0.xsd';
 
     public const MARSHALLER_FACTORY = Qti30MarshallerFactory::class;
 }
