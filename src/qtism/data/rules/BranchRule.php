01eb4be68a727e44233a370062cd847dd650d93a
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-10-03 09:56:18 +0200

    fix: Update license headers: new address and current year
    - Update license address from '51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA'
      to '31 Milk St # 960789 Boston, MA 02196 USA'
    - Update copyright years to include 2025 (current year)
    - Updated files:
      - src/qtism/data/storage/xml/XmlDocument.php
      - src/qtism/data/BranchRuleTargetException.php
      - src/qtism/data/results/AssessmentResult.php
      - src/qtism/data/results/ItemResult.php
      - src/qtism/data/results/ResultResponseVariable.php
      - src/qtism/data/results/TestResult.php

--- src/qtism/data/rules/BranchRule.php
+++ src/qtism/data/rules/BranchRule.php
@@ -13,9 +13,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
  *
- * Copyright (c) 2013-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2025 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @license GPLv2
@@ -54,7 +54,7 @@ class BranchRule extends QtiComponent implements Rule
     public const EXIT_TESTPART = 'EXIT_TESTPART';
     public const EXIT_SECTION = 'EXIT_SECTION';
 
-    public const SPECIAL_TARGETS = [
+    public const RESERVED_TARGETS = [
         self::EXIT_TEST,
         self::EXIT_TESTPART,
         self::EXIT_SECTION,
--- src/qtism/data/storage/xml/XmlDocument.php
+++ src/qtism/data/storage/xml/XmlDocument.php
@@ -13,9 +13,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
  *
- * Copyright (c) 2013-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2025 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  * @author Julien Sébire <julien@taotesting.com>
@@ -170,7 +170,7 @@ class XmlDocument extends QtiDocument
      * @param bool $validate Whether the file must be validated unsing XML Schema? Default is false.
      * @throws XmlStorageException If an error occurs while loading the QTI-XML file.
      */
-    public function load(string $url, ?bool $validate = false): void
+    public function load(string $url, bool $validate = false): void
     {
         $this->loadImplementation($this->loadFromFile($url), $validate);
         $this->setUrl($url);
@@ -705,7 +705,7 @@ class XmlDocument extends QtiDocument
             }
 
             $targetElement = $docComponent->getComponentByIdentifier($target);
-            if ($targetElement === null && !in_array($target, BranchRule::SPECIAL_TARGETS, true)) {
+            if ($targetElement === null && !in_array($target, BranchRule::RESERVED_TARGETS, true)) {
                 $errors[] = sprintf('BranchRule target "%s" does not exist in the document', $target);
             }
         }
